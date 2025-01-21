34be8e7a7f6e1c98a0e757f9bb9d69cb0d1194e6
Alex_H <ewebotah@gmail.com>
2025-01-21 23:19:31 +0100

    Создание проекта

---
+++ src/Lessons/Lesson_17/PartTwo.java
@@ -0,0 +1,9 @@
+package Lessons.Lesson_17;
+
+public class PartTwo {
+    // main psvn
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello!");
+    }
+}
