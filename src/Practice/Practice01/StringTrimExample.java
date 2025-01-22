85473b9ea1a8db7d478b7bf3bb323f74d786371a
Alex_H <ewebotah@gmail.com>
2025-01-22 15:03:32 +0100

    Немного практики

---
+++ src/Practice/Practice01/StringTrimExample.java
@@ -0,0 +1,20 @@
+package Practice.Practice01;
+
+public class StringTrimExample {
+    public static void main(String[] args) {
+        // Задание 2: Удаление лишних пробелов
+        /*
+        Создайте строку с текстом "   Привет, Java!   ".
+        - Удалите пробелы в начале и конце строки.
+        - Выведите результат.
+        */
+        // Создаем строку
+        String text = "   Привет, Java!   ";
+
+        // Удаляем пробелы в начале и конце строки
+        String trimmedText = text.trim();
+
+        // Выводим результат
+        System.out.println(trimmedText);
+    }
+}
