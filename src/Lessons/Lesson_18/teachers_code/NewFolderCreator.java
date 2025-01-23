586d438f50be443edb190abc975382c4cef0e6a3
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-01-23 21:14:34 +0100

    lesson 18
    debug changes

---
+++ src/Lessons/Lesson_18/teachers_code/NewFolderCreator.java
@@ -0,0 +1,29 @@
+package Lessons.Lesson_18.teachers_code;
+
+import java.util.Scanner;
+
+public class NewFolderCreator {
+    public static void main(String[] args) {
+        // input
+        System.out.println("Hello! Enter your name:");
+
+        Scanner myLocalReader = new Scanner(System.in);
+
+        //позволяет считать все введенные данные от начала до конца строки:
+        String name = myLocalReader.nextLine();
+        System.out.printf("Hello, %s\n How old are you?", name);
+
+        int age = myLocalReader.nextInt();
+
+        System.out.println("Have u already celebrate birthday this year? y/n");
+
+        String answer = myLocalReader.nextLine();
+
+        // логическое "и" - "and" Python, "&&" Java
+        // логическое "или" - "or" Python, "||" Java
+
+
+        System.out.printf("You've born in %d\n", (2025-age));
+
+    }
+}
