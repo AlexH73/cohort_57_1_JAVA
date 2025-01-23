8da370e7097bb4955176e26f097100ab2ec2fa33
Alex_H <ewebotah@gmail.com>
2025-01-24 00:15:16 +0100

    Немного практики

--- src/Practice/Practice_18/printMessage.java
+++ src/Practice/Practice_18/printMessage.java
@@ -1,11 +1,22 @@
+//Задание 1: Вывести сообщение
+/*
+ *Код на Python:
+
+ *def print_message():
+ *print("Hello, Python!")
+
+ *print_message()
+ */
 package Practice.Practice_18;
 
 public class printMessage {
     public static void main(String[] args) {
+
         printMessage();
     }
 
     public static void printMessage() {
+
         System.out.println("Hello, Java!");
     }
 }
