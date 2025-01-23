11b571bcc4afce3e19648911b2ba15b245960bb6
Alex_H <ewebotah@gmail.com>
2025-01-23 18:15:10 +0100

    Lesson 18

---
+++ src/Practice/Practice_18/printMessage.java
@@ -0,0 +1,11 @@
+package Practice.Practice_18;
+
+public class printMessage {
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    public static void printMessage() {
+        System.out.println("Hello, Java!");
+    }
+}
