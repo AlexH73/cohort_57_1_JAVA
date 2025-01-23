107416e9372bb13508af1c25649eedad7ac14c22
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-01-23 19:54:24 +0100

    Lesson 18
    package lesson_19 moved to lessons

--- src/Lessons/lesson_18/teachers_code/Lesson18.java
+++ src/Lessons/lesson_18/teachers_code/Lesson18.java
@@ -1,6 +1,7 @@
 package Lessons.lesson_18.teachers_code;
 
 public class Lesson18 {
+    // some changes in main branch
     public static void main(String[] args) {
         System.out.println("Hello there!");
     }
