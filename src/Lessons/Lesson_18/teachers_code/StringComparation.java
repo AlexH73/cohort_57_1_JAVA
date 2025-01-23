d9ac7a43a5bf4e39612f64a76c9544572db06afd
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-01-23 21:44:24 +0100

    lesson 18
    debug changes

--- src/Lessons/Lesson_18/teachers_code/StringComparation.java
+++ src/Lessons/Lesson_18/teachers_code/StringComparation.java
@@ -5,7 +5,9 @@ public class StringComparation {
         // сравнение строк в джава происходит при помощи двух способов:
 
         // при первом способе мы сравниваем строки при помощи "=="
-
+        // при помощи "==" сравниваются исключительно ссылки на ячеки памяти на которые ссылаются переменные,
+        // если значения лежат в разных ячейках, но при жтом одинаковые, то мы все равно получим false,
+        // см случай str1 == str3
         String str1 = "aaa";
         String str2 = "aaa";
         String str3 = new String("aaa");
@@ -16,8 +18,8 @@ public class StringComparation {
         System.out.println("str3 = " + str3);
         System.out.println("str4 = " + str4);
 
-        System.out.println("str1 == str2 = " + str1 == str2);
-        System.out.println("str1 == str3 = " + str1 == str3);
-        System.out.println("str1 == str4 = " + str1 == str4);
+        System.out.println("str1 == str2 = " + (str1 == str2));
+        System.out.println("str1 == str3 = " + (str1 == str3));
+        System.out.println("str1 == str4 = " + (str1 == str4));
     }
 }
