cc3a3d940b88427ca17f985379409179c7ac9aaa
Yulia Kashina <yulia.kaot@mail.ru>
2025-01-26 21:12:54 +0100

    yulia added solutions to homework 18

--- src/Lessons/Lesson_18/teachers_code/NewFolderCreator.java
+++ src/Lessons/Lesson_18/teachers_code/NewFolderCreator.java
@@ -1,3 +1,4 @@
+/*
 package Lessons.Lesson_18.teachers_code;
 
 import java.util.Scanner;
@@ -11,41 +12,13 @@ public class NewFolderCreator {
 
         //позволяет считать все введенные данные от начала до конца строки:
         String name = myLocalReader.nextLine();
-        System.out.printf("Hello, %s\nHow old are you?\n", name);
 
-        // так как nextInt() оставит бегунок считывания сразу после числа, то нам
-        // надо перенести его на начало новой строки, для этого можно разово вызвать:
+        System.out.printf("Hello, %s\n How old are you?", name);
+
         int age = myLocalReader.nextInt();
-        myLocalReader.nextLine();
 
-        // либо, воспользоваться альтернативой:
-        // считать число,как текст и попытаться распарсить его в число
+        System.out.printf("You've born in %d\n", (2025-age));
 
-        // String strAge = myLocalReader.nextLine();
-        // int age = Integer.parseInt(strAge);
-
-        System.out.println("Have u already celebrate birthday this year? y/n");
-
-        String answer = myLocalReader.nextLine();
-
-        // логическое "и" - "and" Python, "&&" Java
-        // логическое "или" - "or" Python, "||" Java
-        // логическое "отрицание" - "not" Python, "!" Java
-
-
-        // здесь ! используется так как нам надо отследить некорректный ввод,
-        //  то есть не y/n
-
-        while (!answer.equals("y") && !answer.equals("n")) {
-            System.out.println("Enter please correct answer y or n");
-            answer = myLocalReader.nextLine();
-        }
-
-
-        if (answer.equals("y") ) {
-            System.out.printf("You've born in %d\n", (2025-age));
-        } else {
-            System.out.printf("You've born in %d\n", (2024-age));
-        }
     }
 }
+*/
