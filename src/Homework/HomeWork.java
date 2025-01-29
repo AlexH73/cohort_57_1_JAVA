5c013ca952f003a9e9be81cf147e133166c2326f
Alex_Berlin <alex.b@tuta.com>
2025-01-29 21:49:29 +0100

    Задача № 2 использование String value = System.getProperty(String key);

---
+++ src/Homework/HomeWork.java
@@ -0,0 +1,40 @@
+package Homework;
+// Домашнее задание: Задача 2 к Лекции 19
+
+// Автор: Alexander Berlin
+// Дата: ${DATE}
+
+import java.sql.SQLOutput;
+
+/**
+ * Описание: * Задача 2: Напишите метод, который выводит на консоль название операционной системы,
+ *      * на которой запущена программа.
+ *      * Пример: printOS() -> "Windows 10"
+ *      *
+ *      * @see System#getProperty(String)
+ **/
+
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println();
+        printOS();
+    }
+    public static void printOS(){
+        String key = "os.version";
+        String value = System.getProperty(key);
+        System.out.println(value);
+
+        String osName = System.getProperty("os.name");
+        System.out.println(osName);
+
+        String userHome = System.getProperty("user.home");
+        System.out.println(userHome);
+
+        String javaVersion = System.getProperty("java.version");
+        System.out.println(javaVersion);
+
+
+    }
+
+}
