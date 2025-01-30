6a0f5da0865d05a846e7a14ad2ea6365ffc770f8
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-01-30 19:38:24 +0100

    lesson 20

---
+++ src/Lessons/Lesson_20/code/theory/ClassWork1.java
@@ -0,0 +1,30 @@
+package Lessons.Lesson_20.code.theory;
+
+import java.io.File;
+
+public class ClassWork1 {
+    public static void main(String[] args) {
+
+        String[] strings = new String[10]; // {null, null, null, null, null, null, null, null, null, null}
+
+        String[] strings1 = {"one", "two", "three"};
+
+        int[] nums = new int[5]; // {0, 0, 0, 0, 0}
+        double[] nums2 = new double[5]; // {0.0, 0.0, 0.0, 0.0, 0.0}
+        boolean[] booleans = new boolean[3]; // {false, false, false}
+        boolean[] booleans2 = {true, false, true};
+
+
+
+        File file = new File("/path/to/file.txt");
+        file = null;
+        String x = null;
+        // 1. config file
+        // 2. host + token
+        // 3. session createSession(host, token)
+        // Session session = createSession(host, token);
+        // session == null ?
+
+
+    }
+}
