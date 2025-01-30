2d29bdde5a6c35badf4126536be08d09f0e8f9c1
Iryna_Haber <gaber.ira@gmail.com>
2025-01-30 19:14:18 +0100

    Homer18work

---
+++ src/Homerwork/Homer18work/GreetingUser.java
@@ -0,0 +1,18 @@
+package Homerwork.Homer18work;
+//Задача 1: Приветствие пользователя
+//Код на Python:
+//name = input("Enter your name: ")
+//print(f"Hello, {name}!")
+import java.util.Scanner;
+
+public class GreetingUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        // Создаем объект Scanner для чтения ввода с клавиатуры
+        System.out.print("Enter your name: ");      // Запрашиваем имя пользователя
+        String name = sc.nextLine();
+        System.out.printf("Hello, " + name + "!" ); // Выводим приветствие
+        sc.close();  //Закрываем сканер
+
+    }
+}
+
