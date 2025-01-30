bcf514245bd2d2ff00ad7ce36eba97ed7efe6664
Alex_H <ewebotah@gmail.com>
2025-01-30 01:23:10 +0100

    Updated

---
+++ src/Lessons/Lesson_19/teachers_code/Lesson19.java
@@ -0,0 +1,33 @@
+package Lessons.Lesson_19.teachers_code;
+
+
+
+public class Lesson19 {
+    //  == #
+
+    /*
+    - многострочный комментарий
+    все что находится между этими символами относится к комментарию
+     */
+
+    /**
+     * Таким образом обычно обозначается документация для кода, то есть то, как этот код использовать.
+     */
+
+    public static void main(String[] args) {
+
+        sayHello();
+    }
+
+
+    public static void sayGoodbye() {
+
+        System.out.println("Goodbye!");
+    }
+
+    public static void sayHello() {
+
+        System.out.println("Hello!");
+
+    }
+}
