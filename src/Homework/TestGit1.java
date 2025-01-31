66b3cdc538cc752b5d200a7b1a6452cd7c80435b
Alex_Berlin <alex.b@tuta.com>
2025-01-31 21:22:59 +0100

    Обновил TestGit.java

---
+++ src/Homework/TestGit1.java
@@ -0,0 +1,17 @@
+package Homework;
+
+// Домашнее задание:
+
+// Автор: Alexander Berlin
+// Дата: ${DATE}
+
+/**
+ * Описание:
+ **/
+ //Создаём каркас класса и метода main (это основа программы)
+public class TestGit1 {
+    public static void main(String[] args) {
+        //Здесь будет вызов метода
+        }
+
+ }
