971c050185e9dd6e4819b08998e9fa97754c9191
Alex_H <ewebotah@gmail.com>
2025-02-02 20:26:30 +0100

    Забыл закрыть scanner + комментарии

--- src/Practice/Practice_19/SimpleConditionalExpression.java
+++ src/Practice/Practice_19/SimpleConditionalExpression.java
@@ -11,19 +11,32 @@ public class SimpleConditionalExpression {
      * Выведите соответствующее сообщение на консоль.<br>
      */
     public static void main(String[] args) {
+        // Запускаем метод для ввода числа и его проверки
         enteringAndVerifying();
     }
 
     public static void enteringAndVerifying() {
+        // Создаем объект Scanner для считывания ввода пользователя
         Scanner scanner = new Scanner(System.in);
+
+        // Просим пользователя ввести число
         System.out.println("Введите число: ");
+
+        // Считываем введенное число
         int number = scanner.nextInt();
+
+        // Проверяем, является ли число отрицательным
         if (number < 0) {
             System.out.println("Отрицательное число = " + number);
+            // Проверяем, является ли число положительным
         } else if (number > 0) {
             System.out.println("Положительное число = " + number);
+            // Если число не меньше нуля и не больше нуля, значит оно равно нулю
         } else {
             System.out.println("Число = " + number);
         }
+
+        // Закрываем сканер, чтобы освободить ресурсы
+        scanner.close();
     }
 }
