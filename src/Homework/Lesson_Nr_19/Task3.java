b6155dfef2976e9549118a8bf1b952ff77b579f0
Alex_Berlin <alex.b@tuta.com>
2025-02-03 20:56:39 +0100

    Решение задачи к уроку 20: Анализ Качества Деталей на Заводе

---
+++ src/Homework/Lesson_Nr_19/Task3.java
@@ -0,0 +1,17 @@
+package Homework.Lesson_Nr_19;
+
+// Домашнее задание:
+
+// Автор: Alexander Berlin
+// Дата: ${DATE}
+
+/**
+ * Описание:
+ **/
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("");
+
+
+    }
+}
