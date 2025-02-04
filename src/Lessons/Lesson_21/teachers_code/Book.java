844c2dc6efae4dc6d3311c85619ad560de2b2973
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-04 21:37:46 +0100

    lesson 21
    добавлен пример домашней работы в директорию lesson_21

--- src/Lessons/Lesson_21/teachers_code/Book.java
+++ src/Lessons/Lesson_21/teachers_code/Book.java
@@ -12,4 +12,11 @@ public class Book {
 
         return info;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" + "Title: " + title  +
+                " Author: " + author  +
+                 "}";
+    }
 }
