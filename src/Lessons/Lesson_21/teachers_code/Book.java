c42e52446e0e273dc011cff10c7e36c1e5bf2f53
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-04 21:34:18 +0100

    lesson 21
    добавлен пример домашней работы в директорию lesson_21

--- src/Lessons/Lesson_21/teachers_code/Book.java
+++ src/Lessons/Lesson_21/teachers_code/Book.java
@@ -4,4 +4,12 @@ public class Book {
     String title;
     String author;
     int pages;
+
+    public String showInfo() {
+        String info = "Title: " + title + "\n" +
+                "Author: " + author + "\n" +
+                "Pages: " + pages;
+
+        return info;
+    }
 }
