2b3f57a41ee3827181326530ddf53d20540b235a
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-04 21:20:42 +0100

    lesson 21
    добавлен пример домашней работы в директорию lesson_21

---
+++ src/Lessons/Lesson_18/teachers_code/Book.java
@@ -0,0 +1,7 @@
+package Lessons.Lesson_18.teachers_code;
+
+public class Book {
+    String title;
+    String author;
+    int pages;
+}
--- src/Lessons/Lesson_21/teachers_code/PenUsage.java
+++ src/Lessons/Lesson_21/teachers_code/PenUsage.java
@@ -8,6 +8,43 @@ public class PenUsage {
 
         System.out.println(iliasPen.getClass());
 
+        Image red = new Image();
+        Image blue = new Image();
+
+        // присвоили значения для полей конкретного экземпляра класса:
+        red.heightPixels = 50;
+        red.widthPixels = 50;
+
+        blue.widthPixels = 30;
+        blue.heightPixels = 70;
+
+
+        System.out.println("red.widthPixels = " + red.widthPixels);
+        System.out.println("red.heightPixels = " + red.heightPixels);
+
+        System.out.println("blue.heightPixels = " + blue.heightPixels);
+        System.out.println("blue.widthPixels = " + blue.widthPixels);
+
+        red.fileName = "Red file";
+        red.fileType = "jpg";
+
+        blue.fileName = "Blue file";
+        blue.fileType = "png";
+
+        System.out.println("red == blue = " + (red == blue));
+        System.out.println("red instanceof Image = " + (red instanceof Image));
+
+
+
+        // MailUser - тип данных
+        // ilias - это конкретный объект
+        // alexander - это конкретный объект
+        MailUser ilias = new MailUser();
+        MailUser alexander = new MailUser();
+
+        // str1 - это конкретный объект
+        String str1 = "hello";
+
 
     }
 }
