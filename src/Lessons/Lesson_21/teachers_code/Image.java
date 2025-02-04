236617890438a2d64e989bf0b8916df9cf9c93d3
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-04 19:49:15 +0100

    lesson 21
    добавлен пример домашней работы в директорию lesson_21

---
+++ src/Lessons/Lesson_21/teachers_code/Image.java
@@ -0,0 +1,46 @@
+package Lessons.Lesson_21.teachers_code;
+
+public class Image {
+    // все, что перечислено ниже называется "поле класса",
+    // в данном примере - поля класса Image
+
+    int heightPixels;
+    int widthPixels;
+    String fileName;
+    String fileType;
+
+
+    /*
+    одномерный массив:
+    [1, 2, 3]
+
+    многомерный массив (в данном случае - двумерный)
+    Image[][] images = new Image[5][4]
+
+
+    [
+        [image1, image2, image3, imageN],
+        [image4, image5, image6, imageN],
+        [image7, image8, image9, imageN],
+        [image10, image11, image12, imageN],
+        [image13, image14, image15, imageN],
+                                                ]
+
+    для image8 - images[2][1]
+    для image12 - images[3][2]
+    для image1 - images[0][0]
+
+
+        int[][] nums =
+
+     {
+        {1},
+        {1, 2},
+        {1, 2, 3}
+                    };
+
+     */
+
+
+
+}
---
+++ src/Lessons/Lesson_21/teachers_code/PenUsage.java
@@ -0,0 +1,13 @@
+package Lessons.Lesson_21.teachers_code;
+
+// класс может называться как угодно,
+// назван он так, только потому что мы хотим тут посмотреть использование класса Pen
+public class PenUsage {
+    public static void main(String[] args) {
+        Pen iliasPen = new Pen();
+
+        System.out.println(iliasPen.getClass());
+
+
+    }
+}
