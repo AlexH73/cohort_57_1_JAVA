a6cd64cd680bd02457a19a301956e969fd8f196d
Alex_H <ewebotah@gmail.com>
2025-02-04 22:20:08 +0100

    Создание объекта

---
+++ src/Practice/Practice_21/AudioTrackUsage.java
@@ -0,0 +1,13 @@
+package Practice.Practice_21;
+
+public class AudioTrackUsage {
+    public static void main(String[] args) {
+        AudioTrack track1 = new AudioTrack();
+
+        track1.artist = "Michael Jackson";
+        track1.genre = "Pop";
+        track1.title = "Thriller";
+        track1.durationInSeconds = 350;
+
+    }
+}
