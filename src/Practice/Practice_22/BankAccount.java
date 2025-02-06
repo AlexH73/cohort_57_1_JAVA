5521dd0eaad3a011a50959dd3eb9c3ea6d3cc8c0
Alex_H <ewebotah@gmail.com>
2025-02-06 16:15:08 +0100

    Подготовил классы для практики

---
+++ src/Practice/Practice_22/BankAccount.java
@@ -0,0 +1,20 @@
+package Practice.Practice_22;
+// TODO: Создать класс BankAccount
+// 1. Добавить поля:
+//    - `accountNumber` (String) - Номер банковского счета
+//    - `balance` (double) - Баланс счета
+// 2. Создать конструктор, который принимает `accountNumber` и начальный баланс.
+// 3. Реализовать методы:
+//    - `deposit(double amount)`, который увеличивает баланс на указанную сумму.
+//    - `withdraw(double amount)`, который уменьшает баланс, если средств достаточно.
+
+class BankAccount {
+// TODO: Определить поля
+
+    // TODO: Реализовать конструктор
+
+    // TODO: Реализовать метод deposit(double amount)
+
+    // TODO: Реализовать метод withdraw(double amount)
+}
+
