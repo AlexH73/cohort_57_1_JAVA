156656e5c35e8bd49a4a5fda19de766720a008b1
Alex_H <ewebotah@gmail.com>
2025-02-06 16:21:06 +0100

    Подготовил классы для ДЗ

---
+++ src/Homework/Homework_22/Order.java
@@ -0,0 +1,21 @@
+package Homework.Homework_22;
+// TODO: Создать класс Order
+// 1. Добавить поля:
+//    - `orderId` (int) - Уникальный идентификатор заказа
+//    - `customerName` (String) - Имя покупателя
+//    - `totalAmount` (double) - Сумма заказа
+//    - `status` (String) - Статус заказа ("Ожидает", "Отправлен", "Доставлен")
+// 2. Создать конструктор, принимающий `orderId`, `customerName`, `totalAmount`.
+// 3. Реализовать методы:
+//    - `updateStatus(String newStatus)`, который меняет статус заказа.
+//    - `displayOrder()`, который выводит информацию о заказе.
+
+class Order {
+// TODO: Определить поля
+
+    // TODO: Реализовать конструктор
+
+    // TODO: Реализовать метод updateStatus(String newStatus)
+
+    // TODO: Реализовать метод displayOrder()
+}
---
+++ src/Homework/Homework_22/Product.java
@@ -0,0 +1,21 @@
+package Homework.Homework_22;
+// TODO: Создать класс Product
+// 1. Добавить поля:
+//    - `name` (String) - Название продукта
+//    - `price` (double) - Цена продукта
+//    - `stock` (int) - Количество на складе
+// 2. Создать конструктор, принимающий все три параметра.
+// 3. Реализовать методы:
+//    - `sell(int quantity)`, который уменьшает `stock`, если товара достаточно на складе.
+//    - `restock(int quantity)`, который увеличивает `stock`.
+
+class Product {
+// TODO: Определить поля
+
+    // TODO: Реализовать конструктор
+
+    // TODO: Реализовать метод sell(int quantity)
+
+    // TODO: Реализовать метод restock(int quantity)
+}
+
