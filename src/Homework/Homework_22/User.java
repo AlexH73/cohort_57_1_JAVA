7c3e8ff4ec79c4cefc9c59d89847909901611570
Alex_H <ewebotah@gmail.com>
2025-02-06 23:59:15 +0100

    done

--- src/Homework/Homework_22/User.java
+++ src/Homework/Homework_22/User.java
@@ -1,17 +1,50 @@
 package Homework.Homework_22;
-// TODO: Создать класс User
-// 1. Добавить поля:
+//[x] TODO: Создать класс User
+//[x] 1. Добавить поля:
 //    - `username` (String) - Логин пользователя
 //    - `email` (String) - Электронная почта
 //    - `age` (int) - Возраст пользователя
-// 2. Создать конструктор, который принимает все три параметра.
-// 3. Реализовать метод `displayUserInfo()`, который выводит информацию о пользователе.
+//[x] 2. Создать конструктор, который принимает все три параметра.
+//[x] 3. Реализовать метод `displayUserInfo()`, который выводит информацию о пользователе.
+
+/**
+ * <h1>Задание 1: Доработка класса User</h1>
+ * 1. Добавить метод isAdult(), который возвращает true, если пользователю 18 лет или больше,
+ * и false в противном случае.<br>
+ * 2. Изменить метод displayUserInfo(), чтобы он также отображал информацию о
+ * совершеннолетии пользователя.<br>
+ * 3. Протестировать класс, создав несколько объектов User и вызвав все методы.
+ */
 
 class User {
-// TODO: Определить поля
+    String username;
+    String email;
+    int age;
 
-    // TODO: Реализовать конструктор
+    // Конструктор для инициализации всех полей
+    public User(String username, String email, int age) {
+        this.age = age;
+        this.email = email;
+        this.username = username;
+        this.isAdult();
+    }
 
-    // TODO: Реализовать метод displayUserInfo()
+    // Метод для проверки, является ли пользователь совершеннолетним
+    public boolean isAdult() {
+        return age >= 18;
+    }
+
+    // Метод для отображения информации о пользователе
+    public void displayUserInfo() {
+        System.out.println("User: " + username + ", \nE-mail: " + email + ", \nAge: " + age);
+        System.out.println("Is adult: " + (isAdult() ? "Yes" : "No"));
+    }
+
+    @Override
+    public String toString() {
+        return "User: {" +
+                "username='" + username + '\'' +
+                '}';
+    }
 }
 
