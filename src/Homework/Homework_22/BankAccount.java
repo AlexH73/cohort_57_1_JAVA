f731aa1858d3fe54062c0cae85c03f5886049090
Alex_H <ewebotah@gmail.com>
2025-02-06 23:07:58 +0100

    done

--- src/Homework/Homework_22/BankAccount.java
+++ src/Homework/Homework_22/BankAccount.java
@@ -1,20 +1,49 @@
 package Homework.Homework_22;
-// TODO: Создать класс BankAccount
-// 1. Добавить поля:
-//    - `accountNumber` (String) - Номер банковского счета
-//    - `balance` (double) - Баланс счета
-// 2. Создать конструктор, который принимает `accountNumber` и начальный баланс.
-// 3. Реализовать методы:
-//    - `deposit(double amount)`, который увеличивает баланс на указанную сумму.
-//    - `withdraw(double amount)`, который уменьшает баланс, если средств достаточно.
 
-class BankAccount {
-// TODO: Определить поля
+public class BankAccount {
+    String accountNumber;
+    double balance;
 
-    // TODO: Реализовать конструктор
+    // Конструктор для инициализации номера счета и начального баланса
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
 
-    // TODO: Реализовать метод deposit(double amount)
+    // Метод для пополнения баланса
+    public void deposit(double amount) {
+        this.balance += amount;
+        System.out.println("Пополнение на " + amount + "€. Остаток средств на балансе: " + balance + "€");
+    }
 
-    // TODO: Реализовать метод withdraw(double amount)
+    // Метод для снятия средств с баланса
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            this.balance -= amount;
+            System.out.println("Снято " + amount + "€ со счета: " + accountNumber);
+            System.out.println("Остаток средств на балансе: " + balance + "€");
+        } else {
+            System.out.println("Не достаточно средств для снятия " + amount + "€. На балансе: " + balance + "€");
+        }
+    }
+
+    // Метод для перевода средств на другой счет
+    public void transfer(BankAccount targetAccount, double amount) {
+        if (balance >= amount) {
+            this.balance -= amount;
+            targetAccount.deposit(amount);
+            System.out.println("Сумма " + amount + "€ переведена на счет: " + targetAccount.accountNumber);
+            System.out.println("Остаток средств на балансе: " + balance + "€");
+        } else {
+            System.out.println("Не достаточно средств для перевода " + amount + "€. На балансе: " + balance + "€");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount {" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance + "€" +
+                '}';
+    }
 }
-
