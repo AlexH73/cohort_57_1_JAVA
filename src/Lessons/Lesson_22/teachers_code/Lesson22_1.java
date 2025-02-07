e2b950bdcc5cfa0991c9dbf01b8ba3f14467d0c1
Alex_H <ewebotah@gmail.com>
2025-02-07 02:52:57 +0100

    Updated

--- src/Lessons/Lesson_22/teachers_code/Lesson22_1.java
+++ src/Lessons/Lesson_22/teachers_code/Lesson22_1.java
@@ -8,10 +8,9 @@ public class Lesson22_1 {
 
         Account ilias_2 = new Account("ilias", "1234", "adas@dcfad.we", "5322, Sdf str 12", 1990);
 
-        Account ilias_3 = new Account("ilias", "1234", "adas@dcfad.we");
+        // Account ilias_3 = new Account("ilias", "1234", "adas@dcfad.we");
 
-
-        Account ilias_4 = new Account();
+       // Account ilias_4 = new Account();
 
 
         System.out.println("ilias_2 = " + ilias_2);
