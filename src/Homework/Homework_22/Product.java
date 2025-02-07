fe6e913dd7c00becf648be72e84ebab4efbede4a
Alex_H <ewebotah@gmail.com>
2025-02-07 01:02:36 +0100

    done

--- src/Homework/Homework_22/Product.java
+++ src/Homework/Homework_22/Product.java
@@ -1,21 +1,66 @@
 package Homework.Homework_22;
-// TODO: Создать класс Product
-// 1. Добавить поля:
+//[x] TODO: Создать класс Product
+//[x] 1. Добавить поля:
 //    - `name` (String) - Название продукта
 //    - `price` (double) - Цена продукта
 //    - `stock` (int) - Количество на складе
-// 2. Создать конструктор, принимающий все три параметра.
-// 3. Реализовать методы:
+//[x] 2. Создать конструктор, принимающий все три параметра.
+//[x] 3. Реализовать методы:
 //    - `sell(int quantity)`, который уменьшает `stock`, если товара достаточно на складе.
 //    - `restock(int quantity)`, который увеличивает `stock`.
 
-class Product {
-// TODO: Определить поля
+/**
+ * <h1>Задание 3: Расширение класса Product</h1>
+ * 1. Добавить метод applyDiscount(double percentage),
+ * который уменьшает цену товара на заданный процент.<br>
+ * 2. Изменить метод sell(int quantity), чтобы он проверял наличие нужного
+ * количества товара; если товара недостаточно, продавать только доступное
+ * количество и выводить сообщение.<br>
+ * 3. Протестировать класс, создав несколько объектов Product и применив скидки и продажи.
+ */
 
-    // TODO: Реализовать конструктор
+public class Product {
+    String name;
+    double price;
+    int stock;
 
-    // TODO: Реализовать метод sell(int quantity)
+    public Product(String name, double price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
 
-    // TODO: Реализовать метод restock(int quantity)
+    // Метод для отображения информации о товаре
+    public void displayProductInfo() {
+        System.out.println("Product: " + name + ", Price: " + price + ", Stock: " + stock);
+    }
+
+    public void sell(int quantity) {
+        if (stock >= quantity) {
+            stock -= quantity;
+            System.out.println("\nSold " + quantity + " units of " + name + ". Remaining stock: " + stock);
+        } else {
+            System.out.println("\nNot enough stock for " + name + ". Available stock: " + stock);
+            System.out.println("Sold " + stock + " units of " + name);
+            stock = 0;
+        }
+    }
+
+    public void applyDiscount(double percentage) {
+        this.price = this.price * (1 - percentage / 100);
+    }
+
+    public void restock(int quantity) {
+        stock += quantity;
+        System.out.println("Restocked " + quantity + " units of " + name + ". Current stock: " + stock);
+    }
+
+    @Override
+    public String toString() {
+        return "Product {" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
 }
-
