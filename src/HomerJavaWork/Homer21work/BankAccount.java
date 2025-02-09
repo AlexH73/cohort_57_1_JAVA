798fa1ae58555c0447d105a0dd3175c9538d0449
Iryna_Haber <gaber.ira@gmail.com>
2025-02-09 15:47:04 +0100

    #добавила файлы

---
+++ src/HomerJavaWork/Homer21work/BankAccount.java
@@ -0,0 +1,24 @@
+package HomerJavaWork.Homer21work;
+
+/*Задание 2: Улучшение класса BankAccount
+Добавить метод transfer(BankAccount targetAccount, double amount), который переводит деньги с текущего счета на другой,
+ если средств достаточно.
+Изменить метод withdraw(double amount), чтобы он не позволял снимать больше, чем есть на счету (выводить сообщение об ошибке).
+Протестировать класс, создав несколько объектов BankAccount и выполнив операции пополнения, снятия и перевода.*/
+public class BankAccount {
+    String accountNumber;
+    double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
