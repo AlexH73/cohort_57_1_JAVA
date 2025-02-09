d3a96799b37ce1ca3ccf36b106de3820a6f456cb
Iryna_Haber <gaber.ira@gmail.com>
2025-02-09 15:47:04 +0100

    #добавила файлы

--- src/HomerJavaWork/Homer21work/Tas3.java
+++ src/HomerJavaWork/Homer21work/Tas3.java
@@ -1,4 +1,31 @@
-package HomerJavaWork.Homer21work;
+/*
+/package HomerJavaWork.Homer21work;
+ */
+/*Задание 3: Расширение класса Product
+Добавить метод applyDiscount(double percentage), который уменьшает цену товара на заданный процент.
+Изменить метод sell(int quantity), чтобы он проверял наличие нужного количества товара; если товара недостаточно, продавать только доступное количество и выводить сообщение.
+Протестировать класс, создав несколько объектов Product и применив скидки и продажи.
+*/
 
-public class Tas3 {
+/*
+ class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public  void applyDiscount(double percentage) {
+
+
+        }
+
+    }
+
 }
+
+/*
