41eccc3d3d43facb1d6f8bb29facde055ce93f7a
Iryna_Haber <gaber.ira@gmail.com>
2025-02-10 20:08:49 +0100

    #добавила файлы домашняя работа 22, Задание 1

--- src/Homework/lesson_22/User.java
+++ src/Homework/lesson_22/User.java
@@ -1,13 +1,12 @@
-package HomerJavaWork.Homer21work;
+package Homework.lesson_22;
 /*Задание 1: Доработка класса User
 Добавить метод isAdult(), который возвращает true, если пользователю 18 лет или больше, и false в противном случае.
 Изменить метод displayUserInfo(), чтобы он также отображал информацию о совершеннолетии пользователя.
 Протестировать класс, создав несколько объектов User и вызвав все методы.
-
  */
 public class User {
-   private String name;
-   private int age;
+    private String name;
+    private int age;
 
     public User(String name, int age) {
         this.name = name;
@@ -16,16 +15,11 @@ public class User {
     public boolean isAdult() {
         return age >= 18;
     }
-   public void displayUserInfo() {
+    public void displayUserInfo() {
         System.out.println("Имя: " + name);
         System.out.println("Возраст: " + age);
         System.out.println("Совершеннолетний: " + (isAdult() ? "Да" : "Нет"));
-   }
-
-
-
-
-
+    }
 
 
 
