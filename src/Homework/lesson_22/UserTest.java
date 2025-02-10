c63a2c47df17621881291f62bae23816e4c819d9
Iryna_Haber <gaber.ira@gmail.com>
2025-02-10 20:11:58 +0100

    #добавила файлы домашняя работа 22, Задание 1

--- src/Homework/lesson_22/UserTest.java
+++ src/Homework/lesson_22/UserTest.java
@@ -2,9 +2,10 @@ package Homework.lesson_22;
 
 public class UserTest {
     public static void main(String[] args) {
-        HomerJavaWork.Homer21work.User user1 = new HomerJavaWork.Homer21work.User("Max" , 19);
-        HomerJavaWork.Homer21work.User user2 = new HomerJavaWork.Homer21work.User("Mary" , 18);
-        HomerJavaWork.Homer21work.User user3 = new User("Pasha" , 16);
+
+        User user1 = new User("Max" , 19);
+        User user2 = new User("Mary" , 18);
+        User user3 = new User("Pasha" , 16);
 
         user1.displayUserInfo();
         System.out.println();
