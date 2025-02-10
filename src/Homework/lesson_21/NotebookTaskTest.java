ec63b353dcae5c9cd472bfe48eab9cdc5076b85d
Iryna_Haber <gaber.ira@gmail.com>
2025-02-10 19:58:43 +0100

    #добавила файлы домашняя работа 21, Задание 3

---
+++ src/Homework/lesson_21/NotebookTaskTest.java
@@ -0,0 +1,29 @@
+package Homework.lesson_21;
+
+import consultations.c10.l21_task_4.Notebook;
+
+public class NotebookTaskTest {
+    public static void main(String[] args) {
+        //Создаем экземпляр Notebook
+        Notebook notebook = new Notebook();
+
+        //Создаем несколько задач с различными приоритетами
+        Task task1 = new Task("Сделать отчет" , 1);
+        Task task2 = new Task("Позвонить клиенту" , 3);
+        Task task3 = new Task("Подготовить презентацию" , 10);
+        Task task4 = new Task("обновить документацию" , 2);
+
+        //Добавляем задачи в блокнот
+        notebook.addTask(task1);
+        notebook.addTask(task2);
+        notebook.addTask(task3);
+        notebook.addTask(task4);
+
+        //Выводим все задачи
+        notebook.showAllTasks();
+
+        //Выводим задачи с высоким приоритетом
+        notebook.showHighPriorityTasks();
+
+    }
+}
