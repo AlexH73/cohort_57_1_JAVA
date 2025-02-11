24d7ec64932602b9e6a69cedcdbf1a00a5ed85c5
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-11 20:31:58 +0100

    Lesson 22
    решение задачи Store

--- src/Lessons/Lesson_23/teachers_code/Lesson23Usage.java
+++ src/Lessons/Lesson_23/teachers_code/Lesson23Usage.java
@@ -1,4 +1,21 @@
 package Lessons.Lesson_23.teachers_code;
 
 public class Lesson23Usage {
+    public static void main(String[] args) {
+        UserAccount account1 = new UserAccount("abc", "1q2w23e");
+
+        System.out.println("account1 = " + account1);
+
+        System.out.println("account1.getLogin() = " + account1.getLogin());
+        account1.setLogin("fgfg");
+
+        System.out.println("account1 = " + account1);
+
+
+
+
+//        System.out.println("account1.login = " + account1.login);
+//        System.out.println("account1.password = " + account1.password);
+
+    }
 }
