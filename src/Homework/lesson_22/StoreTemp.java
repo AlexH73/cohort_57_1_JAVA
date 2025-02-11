7b97c9be0397e8dc63f34c5363f2d4f73cad3bbd
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-11 19:31:04 +0100

    Lesson 22
    решение задачи Store

---
+++ src/Homework/lesson_22/StoreTemp.java
@@ -0,0 +1,63 @@
+package Homework.lesson_22;
+
+
+import java.util.Arrays;
+
+/*
+Создать новый класс Store, который управляет списком товаров (Product). Реализовать:
+
+Поле products (список товаров).
+Метод addProduct(Product product), который добавляет товар в магазин.
+Метод displayAllProducts(), который выводит список всех товаров.
+Метод findMostExpensiveProduct(), который возвращает самый дорогой товар.
+Протестировать, создав Store и добавив несколько товаров.
+ */
+public class StoreTemp {
+    ProductTemp[] products; // null
+
+    public StoreTemp(){
+        this.products = new ProductTemp[0];
+    }
+
+    public StoreTemp(ProductTemp[] products) {
+        this.products = products;
+    }
+
+    public void addProduct(ProductTemp product) {
+        products = Arrays.copyOf(products, products.length + 1);
+        products[products.length - 1] = product;
+    }
+
+    public void displayAllProducts() {
+        for (ProductTemp product : products) {
+            System.out.println(product);
+        }
+
+        // Arrays.toString(products);
+    }
+
+    public ProductTemp findMostExpensiveProduct() {
+        if (products == null || products.length == 0) {
+//            ProductTemp mostExpProduct = null;
+//            return mostExpProduct;
+            return null;
+        }
+
+        // предположим, что первый товар в списке - самый дорогой
+        ProductTemp mostExpProduct = products[0];
+
+        // перебираем все товары в списке продуктов
+        for (int i = 0; i < products.length; i++) {
+            // сравниваем цену самого дорогого на данный момент товара с ценого i-го товара,
+            // если цена i-го товара выше цены самого дорогого из найденных, то значит,
+            // мы нашли более дорогой товар в списке товаров и теперь i-й товар считаем
+            // самым дорогим и будем сравнивать с ним.
+            if (mostExpProduct.price < products[i].price) {
+                mostExpProduct = products[i];
+            }
+        }
+
+        // возвращаем найденный i-й товар, как самый дорогой
+        return mostExpProduct;
+    }
+}
