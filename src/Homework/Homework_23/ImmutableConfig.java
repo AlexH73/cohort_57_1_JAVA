f8577bc113e34a097fa4b71572bdde7b64a6a7e6
Alex_H <ewebotah@gmail.com>
2025-02-12 02:30:40 +0100

    Task 1 done

---
+++ src/Homework/Homework_23/ImmutableConfig.java
@@ -0,0 +1,24 @@
+package Homework.Homework_23;
+
+//[x] TODO: Сделать поля приватными и добавить только геттеры (без сеттеров).
+class ImmutableConfig {
+    private String databaseUrl;
+    private int maxConnections;
+    private boolean isLoggingEnabled;
+
+    // Геттер для databaseUrl
+    public String getDatabaseUrl() {
+        return databaseUrl;
+    }
+
+    // Геттер для maxConnections
+    public int getMaxConnections() {
+        return maxConnections;
+    }
+
+    // Геттер для isLoggingEnabled
+    public boolean getIsLoggingEnabled() {
+        return isLoggingEnabled;
+    }
+}
+
