0d1764a5ee3a69616bb2a6f1dee83b716f3f703e
Alex_H <ewebotah@gmail.com>
2025-02-12 23:17:51 +0100

    Часть 1 готово

---
+++ src/Practice/Practice_23/Book.java
@@ -0,0 +1,38 @@
+package Practice.Practice_23;
+
+//[х] TODO: Добавить геттеры и сеттеры для всех полей.
+public class Book {
+    private String title;
+    private String author;
+    private int pages;
+
+    // Геттер для title
+    public String getTitle() {
+        return title;
+    }
+
+    // Сеттер для title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Геттер для author
+    public String getAuthor() {
+        return author;
+    }
+
+    // Сеттер для author
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Геттер для pages
+    public int getPages() {
+        return pages;
+    }
+
+    // Сеттер для pages
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
---
+++ src/Practice/Practice_23/User.java
@@ -0,0 +1,38 @@
+package Practice.Practice_23;
+
+//[х] TODO: Добавить геттеры и сеттеры для всех полей.
+public class User {
+    private String username;
+    private String email;
+    private int age;
+
+    // Геттер для username
+    public String getUsername() {
+        return username;
+    }
+
+    // Сеттер для username
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Геттер для email
+    public String getEmail() {
+        return email;
+    }
+
+    // Сеттер для email
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Геттер для age
+    public int getAge() {
+        return age;
+    }
+
+    // Сеттер для age
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
