b8ae217c1a69006ba6b02420bde30d3c356009e6
Yulianna-B <juliburkovska@gmail.com>
2025-02-13 19:50:57 +0100

    Classwork24

---
+++ src/Lessons/Lesson_24/teachers_code/smart_device/Camera.java
@@ -0,0 +1,7 @@
+package Lessons.Lesson_24.teachers_code.smart_device;
+
+public class Camera {
+    public void takeaphoto() {
+        System.out.println("Camera made a photo");
+    }
+}
---
+++ src/Lessons/Lesson_24/teachers_code/smart_device/MusicPlayer.java
@@ -0,0 +1,7 @@
+package Lessons.Lesson_24.teachers_code.smart_device;
+
+public class MusicPlayer {
+    public void playMusic(String songname){
+        System.out.println("Play a song" +songname);
+    }
+}
