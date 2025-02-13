06ae33bfabd5c12d931e438fc4fb713314d7eafb
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-13 19:24:22 +0100

    Lesson 24

---
+++ src/Lessons/Lesson_24/teachers_code/animal_example/AnimalUsage.java
@@ -0,0 +1,26 @@
+package Lessons.Lesson_24.teachers_code.animal_example;
+
+public class AnimalUsage {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.voice();
+        animal.feed();
+
+        Dog dog = new Dog();
+        dog.secure();
+        dog.voice();
+        dog.feed();
+
+        Cat cat = new Cat();
+        cat.hunt();
+        cat.feed();
+        cat.voice();
+
+        Animal catAnimal = new Cat();
+
+        //catAnimal.hunt();
+        catAnimal.feed();
+
+        //
+    }
+}
