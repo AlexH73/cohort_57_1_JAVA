1a91f1b97da45ba9e14329576c8e0421b9705b18
Iryna_Haber <gaber.ira@gmail.com>
2025-02-14 19:24:14 +0100

    #добавила файлы домашняя работа 23,Часть3

---
+++ src/Homework/lesson_23/OnlineStoreTest.java
@@ -0,0 +1,18 @@
+package Homework.lesson_23;
+//Основной класс для тестирования
+public class OnlineStoreTest {
+    public static void main(String[] args) {
+        //Создаем объект OnlineStore
+        OnlineStore product = new OnlineStore("Ноутбук", 1500, 20);
+        //Выводим информацию о продукте
+        product.displayProductInfo();
+        // Изменяем скидку (корректное значение)
+        product.setDiscountPercentage(20);
+        System.out.println("\n После изменения скидки:");
+        product.displayProductInfo();
+
+        product.setDiscountPercentage(60);
+        System.out.println("\n После изменения скидки:");
+        product.displayProductInfo();
+    }
+}
