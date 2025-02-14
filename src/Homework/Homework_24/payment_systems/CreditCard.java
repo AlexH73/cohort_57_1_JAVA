b0a48f6d185c650f8cf8349852d44550f2096101
Alex_H <ewebotah@gmail.com>
2025-02-14 04:18:21 +0100

    Part 2 is done

---
+++ src/Homework/Homework_24/payment_systems/CreditCard.java
@@ -0,0 +1,35 @@
+package Homework.Homework_24.payment_systems;
+
+public class CreditCard extends PaymentMethod {
+    private double creditLimit;
+
+    // Конструктор
+    public CreditCard(String accountNumber, double balance, double creditLimit) {
+        super(accountNumber, balance);
+        this.creditLimit = creditLimit;
+    }
+
+    // Геттер и сеттер
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        if (creditLimit < 0) {
+            System.out.println("Лимит кредита не может быть отрицательным.");
+        }
+        this.creditLimit = creditLimit;
+    }
+
+    // Переопределенный метод для снятия средств
+    @Override
+    public void withdrawFunds(double amount) {
+        if (amount < 0) {
+            System.out.println("Сумма не может быть отрицательной.");
+        }
+        if (amount > this.getBalance() + this.creditLimit) {
+            System.out.println("Превышен кредитный лимит.");
+        }
+        this.setBalance(this.getBalance() - amount);
+    }
+}
---
+++ src/Homework/Homework_24/payment_systems/Usage.java
@@ -0,0 +1,33 @@
+package Homework.Homework_24.payment_systems;
+
+public class Usage {
+    public static void main(String[] args) {
+        // Создание объекта PaymentMethod
+        PaymentMethod paymentMethod = new PaymentMethod("123456789", 1000.0);
+        System.out.println("PaymentMethod: " + paymentMethod.getAccountNumber() + ", Balance: " + paymentMethod.getBalance());
+
+        System.out.println();
+        // Создание объекта CreditCard
+        CreditCard creditCard = new CreditCard("987654321", 500.0, 2000.0);
+        System.out.println("CreditCard: " + creditCard.getAccountNumber() + ", Balance: " + creditCard.getBalance() + ", Credit Limit: " + creditCard.getCreditLimit());
+
+        System.out.println();
+        // Создание объекта GoldCreditCard
+        GoldCreditCard goldCreditCard = new GoldCreditCard("555555555", 300.0, 1500.0, 5.0);
+        System.out.println("GoldCreditCard: " + goldCreditCard.getAccountNumber() + ", Balance: " + goldCreditCard.getBalance() + ", Credit Limit: " + goldCreditCard.getCreditLimit() + ", Cashback Percentage: " + goldCreditCard.getCashbackPercentage());
+
+        System.out.println();
+        // Тестирование методов
+        paymentMethod.addFunds(500.0);
+        System.out.println("PaymentMethod Balance после добавления средств: " + paymentMethod.getBalance());
+
+        System.out.println();
+        creditCard.withdrawFunds(100.0);
+        System.out.println("CreditCard Balance после снятия средств: " + creditCard.getBalance());
+
+        System.out.println();
+        double cashback = goldCreditCard.getCashback(200.0);
+        System.out.println("GoldCreditCard Cashback за покупку на 200.0: " + cashback);
+
+    }
+}
