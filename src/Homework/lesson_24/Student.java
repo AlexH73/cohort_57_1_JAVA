38e3cb57c45b6ccd701d267adc0a2dfd21619021
Iryna_Haber <gaber.ira@gmail.com>
2025-02-15 17:50:38 +0100

    #добавила файлы домашняя работа 24,Часть2

---
+++ src/Homework/lesson_24/Student.java
@@ -0,0 +1,22 @@
+package Homework.lesson_24;
+
+public class Student extends User {
+    private String studentId;
+
+    //Конструктор
+    public Student(String username, String email, String studentId) {
+        super(username, email);
+        this.studentId = studentId;
+    }
+    //Геттер
+    public String getStudentId() {
+        return studentId;
+    }
+    //Переопределение метода для отображения информации
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("ID студента: " + studentId);
+    }
+
+}
