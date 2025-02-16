27a172e64dd4cb0c3d8c316b4ef6994778d3f102
Iryna_Haber <gaber.ira@gmail.com>
2025-02-16 15:02:25 +0100

    #добавила файлы домашняя работа 24,Часть2,Задача1

---
+++ src/Homework/lesson_24/UserHierarchyLearning/UserTest.java
@@ -0,0 +1,24 @@
+package Homework.lesson_24.UserHierarchyLearning;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User("Ivan",
+                "ivan@example.com");
+        user.displayInfo();
+
+        System.out.println();
+
+        Student student = new Student("Alex",
+                "alex@example.com", "789");
+        student.displayInfo();
+
+        System.out.println();
+
+        PremiumStudent premiumStudent = new PremiumStudent("Masha",
+                "masha@example.com", "250", "Gold");
+        premiumStudent.displayInfo();
+
+        System.out.println();
+
+    }
+}
