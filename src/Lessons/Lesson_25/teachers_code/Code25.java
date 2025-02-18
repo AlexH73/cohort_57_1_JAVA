536791a348d6c0de6efc9c6af35413c0412d062b
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-18 18:51:18 +0100

    Lesson 25 OOP

--- src/Lessons/Lesson_25/teachers_code/Code25.java
+++ src/Lessons/Lesson_25/teachers_code/Code25.java
@@ -3,5 +3,6 @@ package Lessons.Lesson_25.teachers_code;
 public class Code25 {
     public static void main(String[] args) {
         System.out.println("hello lesson 25");
+        System.out.println("hello lesson 25.2");
     }
 }
