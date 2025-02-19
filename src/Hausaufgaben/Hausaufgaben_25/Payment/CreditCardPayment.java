3988434e95f35f103e1f6f52f87c450e4bc48ba1
Götte <gott_ii@gmx.de>
2025-02-19 13:31:34 +0100

    Hausaufgaben_25 Angefangen

---
+++ src/Hausaufgaben/Hausaufgaben_25/Payment/CreditCardPayment.java
@@ -0,0 +1,10 @@
+package Hausaufgaben.Hausaufgaben_25.Payment;
+
+public class CreditCardPayment extends PaymentMethod{
+    private String creditCardNumber;
+
+    public CreditCardPayment(double amount, String currency, String creditCardNumber) {
+        super(amount, currency);
+        this.creditCardNumber = creditCardNumber;
+    }
+}
