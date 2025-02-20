60a2e53192d6a6b654ee708b9eb980cc2a33e1f8
Alex_H <ewebotah@gmail.com>
2025-02-20 20:17:49 +0100

    внес правки

--- src/Lessons/Lesson_26/student_code/Person.java
+++ src/Lessons/Lesson_26/student_code/Person.java
@@ -1,19 +1,22 @@
 package Lessons.Lesson_26.student_code;
 
 public class Person {
-    private int age;
     private String name;
+    private int age;
     private String phoneNumber;
 
-    public Person(int age, String name, String phoneNumber) {
-        this.age = age;
+    public Person(String name, int age, String phoneNumber) {
         this.name = name;
+        this.age = age;
         this.phoneNumber = phoneNumber;
     }
-
 
     public String getName() {
         return name;
     }
 
+    @Override
+    public String toString() {
+        return name + ", " + age + " лет, Тел: " + phoneNumber;
+    }
 }
