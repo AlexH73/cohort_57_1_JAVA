ae5046cb83485fb18022cef843b254a1eca932d4
Iryna_Haber <gaber.ira@gmail.com>
2025-02-20 21:58:50 +0100

    #Класс MedicalHistory почистила

--- src/Lessons/Lesson_26/student_code/MedicalHistory.java
+++ src/Lessons/Lesson_26/student_code/MedicalHistory.java
@@ -2,8 +2,6 @@ package Lessons.Lesson_26.student_code;
 
 import java.util.Arrays;
 
-/*✔ MedicalHistory – хранит список Appointment для конкретного животного.
-✔ Метод addAppointment(), который добавляет новый визит питомца в историю.*/
 public class MedicalHistory {
     private Animal animal;
     private Appointment[] appointments;
@@ -12,6 +10,7 @@ public class MedicalHistory {
         this.animal = animal;
         this.appointments = appointments;
     }
+
     public MedicalHistory(Animal animal, Appointment appointment) {
         this.animal = animal;
         this.appointments = new Appointment[1];
@@ -19,10 +18,7 @@ public class MedicalHistory {
     }
 
     public void addAppointment(Appointment appointment) {
-        //int[] newArray = Arrays.copyOf(originalArray, newLength);
         appointments = Arrays.copyOf(appointments, appointments.length + 1);
         appointments[appointments.length - 1] = appointment;
     }
-
-
 }
