9e098f1d6762f394bd2092b89d2f88cb9346e956
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-23 19:04:27 +0100

    homework 26 teachers fix

--- src/Hausaufgaben/Hausaufgaben_26/SimpleCounter.java
+++ src/Hausaufgaben/Hausaufgaben_26/SimpleCounter.java
@@ -2,16 +2,15 @@ package Hausaufgaben.Hausaufgaben_26;
 
 public class SimpleCounter {
     public void count() {
-        for(int i = 1;
-            i <= 10;
-            i++) {
-            if(i%2 == 0)
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0)
                 System.out.println(i + " is even");
             else
                 System.out.println(i + " is odd");
         }
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         SimpleCounter sc = new SimpleCounter();
         sc.count();
     }
