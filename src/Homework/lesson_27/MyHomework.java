2be2763add0ad97e005eb76ba69cf6904311aa55
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-25 18:43:27 +0100

    homework 27

--- src/Homework/lesson_27/MyHomework.java
+++ src/Homework/lesson_27/MyHomework.java
@@ -1,7 +1,8 @@
 package Homework.lesson_27;
 
+// не оответствует заданию
 public class MyHomework {
+    // удаляйте неиспользуемые и неосмысленные комментарии:
     // kahsbdkds
-
     // krnfkejfnedf
 }
