4b7edb52ecda987e0ecb9970af418b301dffa073
Yulia Kashina <yulia.kaot@mail.ru>
2025-02-25 20:35:18 +0100

    Class Main and VetClinic created

---
+++ src/Lessons/Lesson_26/student_code/Main.java
@@ -0,0 +1,14 @@
+// package Lessons.Lesson_26.student_code;
+
+//public class Main {
+//   public static void main(String[] args) {
+
+//       VetClinic clinic = new VetClinic("Иванов", "Müller", "вторник");
+
+
+
+
+
+
+//   }
+//}
---
+++ src/Lessons/Lesson_26/student_code/VetClinic.java
@@ -0,0 +1,41 @@
+/*
+package Lessons.Lesson_26.student_code;
+
+public class VetClinic {
+
+   private String listClient;
+   private String listVeterinarian;
+   private String listAppointment;
+
+    public VetClinic(String listClient, String listVeterinarian, String listAppointment) {
+        this.listClient = listClient;
+        this.listVeterinarian = listVeterinarian;
+        this.listAppointment = listAppointment;
+    }
+
+    public void registerClient(Client client) {
+
+
+    }
+
+
+    public void addPetToClient(Client client, Animal pet) {
+
+    }
+
+
+    public void scheduleAppointment(Animal animal, Veterinarian veterinarian, String date) {
+
+    }
+
+
+    public void getMedicalHistory(Animal animal) {
+
+    }
+
+
+    public void showAllAppointments() {
+
+    }
+}
+*/
