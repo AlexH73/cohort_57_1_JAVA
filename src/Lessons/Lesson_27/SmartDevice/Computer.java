72f6a30287c78f9e83e3f40bb82d459dfea35e06
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-25 19:23:45 +0100

    homework 27

--- src/Lessons/Lesson_27/SmartDevice/Computer.java
+++ src/Lessons/Lesson_27/SmartDevice/Computer.java
@@ -1,6 +1,6 @@
 package Lessons.Lesson_27.SmartDevice;
 
-public class Computer {
+public class Computer extends Camera {
     public void connectToTheInternet() {
         System.out.println("Computer connected to the internet");
     }
--- src/Lessons/Lesson_27/SmartDevice/Phone.java
+++ src/Lessons/Lesson_27/SmartDevice/Phone.java
@@ -1,6 +1,6 @@
 package Lessons.Lesson_27.SmartDevice;
 
-public class Phone {
+public class Phone extends MusicPlayer {
     public void makeACall() {
         System.out.println("Phone make a call");
     }
