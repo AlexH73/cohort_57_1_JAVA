c2382e662093e3a7a0e138f0ffe8fc3a24373f66
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-02-25 19:44:13 +0100

    homework 27

--- src/Lessons/Lesson_27/SmartDevice/SmartDevice.java
+++ src/Lessons/Lesson_27/SmartDevice/SmartDevice.java
@@ -2,6 +2,15 @@ package Lessons.Lesson_27.SmartDevice;
 
 public class SmartDevice implements CameraInterface {
     public static void main(String[] args) {
+        SmartDevice device = new SmartDevice();
+        //device.takeAPhoto();
+
+        // пример полиморфизма, когда мы берем один тип данных и реализацию через другой
+        CameraInterface camera1 = new SmartDevice();
+        CameraInterface camera2 = new OldCamera();
+
+        camera1.takeAPhoto();
+        camera2.takeAPhoto();
     }
 
     @Override
