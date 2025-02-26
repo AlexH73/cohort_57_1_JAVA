ac5ec44c3407113df3517f5b74b6af02c1a84203
Götte <gott_ii@gmx.de>
2025-02-26 12:48:29 +0100

    Hausaufgaben_27 angefangen

---
+++ src/Hausaufgaben/Hausaufgaben_27/Connectable.java
@@ -0,0 +1,20 @@
+package Hausaufgaben.Hausaufgaben_27;
+
+public interface Connectable {
+    /**
+     * Подключает устройство к Wi-Fi сети.
+     * @param network Название сети.
+     */
+    void connectToWiFi(String network);
+
+    /**
+     * Отключает устройство от сети Wi-Fi.
+     */
+    void disconnectFromWiFi();
+
+    /**
+     * Проверяет статус подключения.
+     * @return true, если подключено, false – в противном случае.
+     */
+    boolean checkConnectionStatus();
+}
