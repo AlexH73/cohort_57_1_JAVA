4339e9939a9d8f40c02090be18f46c7cae22e9af
Yulia Kashina <yulia.kaot@mail.ru>
2025-02-27 15:15:05 +0100

    HomeworkYulia 27 is done.

---
+++ src/Homework/lesson_27/HomeworkYulia_27/SmartThermostat.java
@@ -0,0 +1,32 @@
+package Homework.lesson_27.HomeworkYulia_27;
+
+public class SmartThermostat implements Connectable {
+    private int currentTemperature;
+    private int targetTemperature;
+    private boolean isConnected;
+
+    @Override
+    public void connectToWiFi(String network) {
+        isConnected = true;
+        System.out.println("Умный термостат подключен к сети " + network);
+    }
+
+    @Override
+    public void disconnectFromWiFi() {
+        isConnected = false;
+        System.out.println("Умный термостат отключен от Wi-Fi");
+    }
+
+    @Override
+    public boolean checkConnectionStatus() {
+        return isConnected;
+    }
+
+    public void setTemperature(int targetTemp) {
+        System.out.println("Желаемая температура установлена на " + targetTemperature);
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+}
