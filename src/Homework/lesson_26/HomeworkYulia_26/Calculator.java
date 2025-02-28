51ca5b9c76c700be1457fbc89bb0d626052877aa
Yulia Kashina <yulia.kaot@mail.ru>
2025-02-28 10:05:32 +0100

    HomeworkYulia 26 is done.

---
+++ src/Homework/lesson_26/HomeworkYulia_26/Calculator.java
@@ -0,0 +1,19 @@
+package Homework.lesson_26.HomeworkYulia_26;
+
+public class Calculator {
+
+    public int addNumbers(int[] nums){
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        int result = calc.addNumbers(new int[]{1, 2, 3, 4});
+        System.out.println("Сумма: " + result);
+    }
+}
+
