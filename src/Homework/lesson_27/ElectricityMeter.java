a79ff860ec7348d00bbee7670d686f730af43939
Alex_H <ewebotah@gmail.com>
2025-03-01 21:16:06 +0100

    Updated

--- src/Homework/lesson_27/ElectricityMeter.java
+++ src/Homework/lesson_27/ElectricityMeter.java
@@ -1,5 +1,8 @@
 package Homework.lesson_27;
 
+/**
+ * Интерфейс для учёта энергопотребления.
+ */
 public interface ElectricityMeter {
     String measurement = "kWh";
     double energyConsumption();
