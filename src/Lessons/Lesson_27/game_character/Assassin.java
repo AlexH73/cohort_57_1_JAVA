d4d5c0d475019a4f5dda595809a1a37c85d76278
Alex_H <ewebotah@gmail.com>
2025-03-04 21:47:53 +0100

    Рефакторинг системы персонажей и добавление новых возможностей
    - Добавлен интерфейс Targetable для управления целями персонажей
    - Реализован базовый класс BaseGameCharacter с общей логикой:
      * Управление здоровьем
      * Система целей
      * Защита от отрицательного HP
    - Полностью переработан класс Archer:
      * Добавлена система ограниченных стрел
      * Реализована логика выбора атак (ближняя/дальняя)
      * Визуализация действий с эмодзи
    - Улучшена документация:
      * Подробные JavaDoc комментарии
      * Пояснения логики работы методов
      * Описание принципов ООП в реализации
    - Исправление критических ошибок:
      * Обработка NullPointerException
      * Проверки на null целей
      * Корректное уменьшение боеприпасов
    - Добавлена система расширяемости:
      * Легкое добавление новых персонажей
      * Гибкая настройка характеристик
      * Возможность развития способностей
    
    Теперь персонажи обладают:
    - Автоматическим выбором целей
    - Разными типами атак
    - Ограниченными ресурсами
    - Полиморфным поведением
    - Защитой от некорректных состояний

---
+++ src/Lessons/Lesson_27/game_character/Assassin.java
@@ -0,0 +1,48 @@
+package Lessons.Lesson_27.game_character;
+
+/**
+ * Класс ассасина с комбинированными способностями
+ */
+public class Assassin extends BaseGameCharacter implements Skill, Stealthy, Ranged {
+    private boolean isStealthMode = true;
+
+    public Assassin(String name) {
+        super(name);
+        System.out.println("🗡️ Ассасин " + name + " вышел на охоту!");
+    }
+
+    @Override
+    public void performAction() {
+        if (isStealthMode) {
+            stealthyAttack(null);
+        } else {
+            rangedAttack(null);
+        }
+        isStealthMode = !isStealthMode;
+    }
+
+    @Override
+    public void stealthyAttack(GameCharacter target) {
+        System.out.println("👤️ " + name + " наносит смертоносный удар из тени!");
+        if (target != null) {
+            target.takeDamage(40);
+        }
+    }
+
+    @Override
+    public void rangedAttack(GameCharacter target) {
+        System.out.println("🎯 " + name + " бросает отравленный клинок!");
+        if (target != null) {
+            target.takeDamage(25);
+        }
+    }
+
+    @Override
+    public void attack(GameCharacter target) {
+        if (Math.random() < 0.7) {
+            stealthyAttack(target);
+        } else {
+            rangedAttack(target);
+        }
+    }
+}
