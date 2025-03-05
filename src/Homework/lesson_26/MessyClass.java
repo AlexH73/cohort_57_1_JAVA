834f40dfd9255426fc680253c334ff10f506d3d1
Iryna_Haber <gaber.ira@gmail.com>
2025-03-05 12:38:14 +0100

    #Домашнее задание26 готово

---
+++ src/Homework/lesson_26/MessyClass.java
@@ -0,0 +1,26 @@
+package Homework.lesson_26;
+
+public class MessyClass {
+    public static final int MAX_VALUE = 100; // константа (можно оставить, но ниже покажем нарушения)
+
+    // Переменная с нижним подчеркиванием (по стандарту должно быть myVar)
+    private int myVar = 10;
+
+    // Метод с неправильным именованием (по стандарту - lowerCamelCase)
+    public void DoSomething() {
+        System.out.println("Doing something...");
+    }
+
+    // Метод с плохими отступами и некорректным именованием (должно быть calculateSum)
+    public int calculateSum(int a, int b) {
+        int sum = a + b;  // неправильный отступ и отсутствие пробелов вокруг оператора +
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        MessyClass obj = new MessyClass();
+        obj.DoSomething();
+        int result = obj.calculateSum(5, 10);
+        System.out.println("Result: " + result);
+    }
+}
---
+++ src/Homework/lesson_26/SimpleCounter.java
@@ -0,0 +1,17 @@
+package Homework.lesson_26;
+
+public class SimpleCounter {
+    public void count() {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0)
+                System.out.println(i + " is even");
+            else
+                System.out.println(i + " is odd");
+        }
+    }
+
+    public static void main(String args[]) {
+        SimpleCounter sc = new SimpleCounter();
+        sc.count();
+    }
+}
