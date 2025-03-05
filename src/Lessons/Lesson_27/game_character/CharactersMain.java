ff90733d511fbfc4b756ce4a01c3c55778a21245
Götte <gott_ii@gmx.de>
2025-03-05 12:24:51 +0100

    Hausaufgaben_27 CharactersMain geendert

--- src/Lessons/Lesson_27/game_character/CharactersMain.java
+++ src/Lessons/Lesson_27/game_character/CharactersMain.java
@@ -3,17 +3,17 @@ package Lessons.Lesson_27.game_character;
 public class CharactersMain {
     public static void main(String[] args) {
         GameCharacter[] characters = {
-                new Archer(),
-                new Assassin(),
-                new Mage(),
-                new Rogue(),
-                new Warrior(),
-                new Priest()
+                new Archer(null),
+                new Assassin(null),
+                new Mage(null),
+                new Rogue(null),
+                new Warrior(null),
+                new Priest(null)
         };
         for (GameCharacter character : characters) {
             character.getName();
             character.getHealth();
-            character.takeDamage();
+            character.takeDamage(0);
             character.getHealth();
             System.out.println();
         }
