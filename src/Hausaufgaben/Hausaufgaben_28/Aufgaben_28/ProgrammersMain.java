dee13c7527fe3590dba21d1bab32b18ef667a3db
Alexander_Gott <gott_ii@gmx.de>
2025-03-07 23:09:44 +0100

    A_28 Hausaufgabe beendet

--- src/Hausaufgaben/Hausaufgaben_28/Aufgaben_28/ProgrammersMain.java
+++ src/Hausaufgaben/Hausaufgaben_28/Aufgaben_28/ProgrammersMain.java
@@ -1,11 +1,15 @@
 package Hausaufgaben.Hausaufgaben_28.Aufgaben_28;
 
+import Hausaufgaben.Hausaufgaben_28.Untericht_28_3.Manager;
+
+import java.awt.*;
 import java.util.Scanner;
 
 public class ProgrammersMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-         ProgrammersList programmersList = createProgrammersList(scanner);
+
+        ProgrammersList programmersList = createProgrammersList(scanner);
         do {
             System.out.println("Хотите добавить в команду программиста (Y/N)? ");
             String answer = scanner.nextLine();
@@ -14,17 +18,21 @@ public class ProgrammersMain {
             }
             Programmer programmer = ProgrammersMain.readProgrammer(scanner);
             programmersList.addProgrammer(programmer);
+
         }while (true);
         System.out.println("-----------------------");
         System.out.println(programmersList);
     }
+    public static ProgrammersList createProgrammersList(Scanner scanner){
+        return new ProgrammersList();
+    }
 
-    public static readProgrammer(Scanner scanner){
+    public static Programmer readProgrammer(Scanner scanner) {
         System.out.println("введите имя программиста");
         String name = scanner.nextLine();
         System.out.println("язык программирования:");
-        String language = scanner.nextLine();
-        return new Programmer(name,technologies);
+        String technologies  = scanner.nextLine();
+        return new Programmer(name, null);
 
     }
 }
