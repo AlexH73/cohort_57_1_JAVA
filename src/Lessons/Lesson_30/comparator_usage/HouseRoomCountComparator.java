573f41c762a0e358250e3105182bd221d74bf02c
Alexander_Gott <gott_ii@gmx.de>
2025-03-11 19:44:37 +0100

    HouseRoomCountComparator beendet

--- src/Lessons/Lesson_30/comparator_usage/HouseRoomCountComparator.java
+++ src/Lessons/Lesson_30/comparator_usage/HouseRoomCountComparator.java
@@ -1,4 +1,10 @@
 package Lessons.Lesson_30.comparator_usage;
 
-public class HouseRoomCountComparator {
+import java.util.Comparator;
+
+public class HouseRoomCountComparator implements Comparator<House> {
+    @Override
+    public int compare(House house1, House house2) {
+        return house1.getRoomCount() - house2.getRoomCount();
+    }
 }
