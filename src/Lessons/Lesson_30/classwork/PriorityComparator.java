869caab154f19cb9f35a71729cab83131e0ba536
Yulianna-B <juliburkovska@gmail.com>
2025-03-11 22:03:29 +0100

    PriorityComparator beendet

--- src/Lessons/Lesson_30/classwork/PriorityComparator.java
+++ src/Lessons/Lesson_30/classwork/PriorityComparator.java
@@ -1,4 +1,17 @@
 package Lessons.Lesson_30.classwork;
 
-public class PriorityComparator {
+import java.util.Comparator;
+
+public class PriorityComparator implements Comparator<Task> {
+
+    @Override
+    public int compare(Task o1, Task o2) {
+        if (o1.getPriority() > o2.getPriority()) {
+            return 1;
+        } else if (o1.getPriority() < o2.getPriority()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
