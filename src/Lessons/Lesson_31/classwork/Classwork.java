583d36bbc7d9d6b43b1a306ba153629c4b551e19
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-03-13 19:30:38 +0100

    добавлен урок 31
    пример equals и hashcode

--- src/Lessons/Lesson_31/classwork/Classwork.java
+++ src/Lessons/Lesson_31/classwork/Classwork.java
@@ -2,16 +2,10 @@ package Lessons.Lesson_31.classwork;
 
 import Lessons.Lesson_30.comparator_usage.House;
 
+import java.util.*;
+
 public class Classwork {
     public static void main(String[] args) {
-        String str1 = new String("Hello");
-        String str2 = new String("Hello");
-
-        System.out.println("str1 == str2 = " + (str1 == str2));
-        System.out.println("str1.equals(str2) = " + str1.equals(str2));
-        System.out.println("str1.hashCode() = " + str1.hashCode());
-        System.out.println("str2.hashCode() = " + str2.hashCode());
-
         House house5 = new House(11, 5, 2000, 110);
         House house6 = new House(11, 90, 2000, 110);
 
@@ -20,5 +14,18 @@ public class Classwork {
 
         System.out.println("house5 == house6 = " + (house5 == house6));
         System.out.println("house5.equals(house6) = " + house5.equals(house6));
+
+        List<House> houses = new ArrayList<>();
+        houses.add(house5);
+        houses.add(house6);
+        System.out.println("houses.size() = " + houses.size());
+
+        Set<House> houseSet = new HashSet<>();
+
+        houseSet.add(house5);
+        houseSet.add(house6);
+        System.out.println("houseSet.size() = " + houseSet.size());
+
+
     }
 }
