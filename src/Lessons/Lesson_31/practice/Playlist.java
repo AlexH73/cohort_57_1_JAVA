8b9d79f09d752716e36ac60fdb79dd9b9f844152
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-03-13 21:02:10 +0100

    добавлен урок 31
    пример equals и hashcode

---
+++ src/Lessons/Lesson_31/practice/Playlist.java
@@ -0,0 +1,13 @@
+package Lessons.Lesson_31.practice;
+
+/**
+ * Создайте коллекцию типа Set<Song> (например, HashSet<Song>), чтобы хранить песни.
+ *
+ * Добавьте несколько песен в плейлист, в том числе попытайтесь
+ * добавить дубликаты, и убедитесь, что они не сохраняются.
+ */
+public class Playlist {
+    public static void main(String[] args) {
+
+    }
+}
