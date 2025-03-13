7b34c66a52e2978ab304f160b776c4d21bd20a86
Iryna_Haber <gaber.ira@gmail.com>
2025-03-13 22:00:26 +0100

    #Playlist

--- src/Lessons/Lesson_31/practice/Playlist.java
+++ src/Lessons/Lesson_31/practice/Playlist.java
@@ -1,5 +1,8 @@
 package Lessons.Lesson_31.practice;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Создайте коллекцию типа Set<Song> (например, HashSet<Song>), чтобы хранить песни.
  *
@@ -8,6 +11,20 @@ package Lessons.Lesson_31.practice;
  */
 public class Playlist {
     public static void main(String[] args) {
+        Set<Song> playList = new HashSet<Song>();
+
+        Song song1 = new Song("Звездное лето", "Пугачева");
+        Song song2 = new Song("Белые розы", "Ласковый май");
+        Song song3 = new Song("Звездное лето", "Пугачева");
+
+        playList.add(song1);
+        playList.add(song2);
+        playList.add(song3);
+
+        System.out.println(playList.size());
+
+        System.out.println(playList);
+
 
     }
 }
