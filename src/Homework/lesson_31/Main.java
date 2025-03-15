dad3423688c3a061f8428c316a8fe079d6639cd2
Alex_H <ewebotah@gmail.com>
2025-03-15 01:10:11 +0100

    Updated
    Обработки null-значений.
    Добавления тестов для граничных случаев.

--- src/Homework/lesson_31/Main.java
+++ src/Homework/lesson_31/Main.java
@@ -1,6 +1,7 @@
 package Homework.lesson_31;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -22,6 +23,10 @@ public class Main {
 
         System.out.println("\n=== ТЕСТИРОВАНИЕ СТРОК ===");
         testStringOperations(operations, fruitsWithDuplicates, fruits1, fruits2);
+
+        System.out.println("\n=== ТЕСТИРОВАНИЕ ПУСТЫХ СПИСКОВ ===");
+        System.out.println("Уникальны ли []? -> " + (operations.areIntegerElementsUnique(Collections.emptyList())? "Да" : "Нет"));
+        System.out.println("Объединение пустых списков: " + operations.unionOfIntegerLists(Collections.emptyList(), Collections.emptyList()));
     }
 
     private static void testIntegerOperations(CollectionOperationsHomework ops,
