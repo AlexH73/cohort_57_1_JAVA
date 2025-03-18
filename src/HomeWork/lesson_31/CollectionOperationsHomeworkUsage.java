3670e005cbbb732567f6cba48ba73484fb9be27a
Juri_Bürkle <rastopka@gmx.degit config --global user.email rastopka@gmx.de>
2025-03-18 18:28:28 +0100

    update

---
+++ src/HomeWork/lesson_31/CollectionOperationsHomeworkUsage.java
@@ -0,0 +1,10 @@
+/*
+package Homework.lesson_31;
+
+public class CollectionOperationsHomeworkUsage {
+    public static void main(String[] args) {
+        CollectionOperationsHomework usage = new CollectionOperationsHomework();
+
+    }
+}
+*/
