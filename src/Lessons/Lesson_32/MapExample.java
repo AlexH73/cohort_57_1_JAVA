41458fef1ad6d4e0adb33715f9a366adb6e26f80
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-03-18 19:56:31 +0100

    добавлен урок 31
    пример equals и hashcode

---
+++ src/Lessons/Lesson_32/MapExample.java
@@ -0,0 +1,38 @@
+package Lessons.Lesson_32;
+
+import java.util.*;
+
+public class MapExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+
+        Map<String, Integer> map =  new HashMap<>();
+
+        map.put("France", 2019);
+        map.put("India", 2007);
+        map.put("Japan", 1994);
+        map.put("Finland", 2011);
+        map.put("Canada", 2014);
+        map.put("Kenia", 2014);
+
+        printMap(map);
+
+        map.put("Finland", 2025);
+        printMap(map);
+
+        Integer japanVisitedAt = map.get("Japan");
+        System.out.println(japanVisitedAt);
+
+        Integer norwayVisitedAt = map.get("Norway");
+        System.out.println(norwayVisitedAt);
+
+        System.out.println("map.containsKey(\"Norway\") = " + map.containsKey("Norway"));
+        System.out.println("map.containsKey(\"Japan\") = " + map.containsKey("Japan"));
+
+    }
+
+
+    private static void printMap(Map map) {
+        System.out.println(map);
+    }
+}
