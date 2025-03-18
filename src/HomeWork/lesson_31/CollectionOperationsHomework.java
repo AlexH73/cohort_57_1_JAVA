5c4104ef7f5f787474e2feb808694969913e81f4
Juri_Bürkle <rastopka@gmx.degit config --global user.email rastopka@gmx.de>
2025-03-18 18:29:27 +0100

    update

---
+++ src/HomeWork/lesson_31/CollectionOperationsHomework.java
@@ -0,0 +1,72 @@
+/*
+package Homework.lesson_31;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public interface CollectionOperationsHomework {
+
+    @Override
+    int countDuplicatesInteger(List<Integer> list);
+    Set<Integer> uniqueSet = new HashSet<>(list);
+    return list.size() - uniqueSet.size();
+
+    @Override
+    List<Integer> intersectionOfIntegerList(List<Integer> list1, List<Integer> list2);
+    Set<Integer> unionSet = new HashSet<>(list1);
+    unionSet .addAll(list2);
+    return new ArrayList<>(unionSet);
+
+    @Override
+    ist<Integer> intersectionOfIntegerList(List<Integer> list1, List<Integer> list2);
+    Set<Integer> set1 = new HashSet<>(list1);
+    Set<Integer> set2 = new HashSet<>(list2);
+    set1.retainAll(set2);
+    return new ArrayList<>(set1);
+
+    @Override
+    ist<Integer> differenceOfIntegerList(List<Integer> list1, List<Integer> list2);
+    Set<Integer> set1 = new HashSet<>(list1);
+    Set<Integer> set2 = new HashSet<>(list2);
+    set1.remoweAll(set2);
+    return new ArrayList<>(set1);
+
+    @Override
+    boolean areIntegerElementsUnique(List<Integer> list);
+    Set<Integer> uniqueSet = new HashSet<>(list);
+    return list.size() == uniqueSet.size();
+
+    @Override
+    int countDuplicatesString(List<Integer> list);
+    Set<Integer> uniqueSet = new HashSet<>(list);
+    return list.size() - uniqueSet.size();
+
+    @Override
+    List<Integer> intersectionOfStringList(List<Integer> list1, List<Integer> list2);
+    Set<Integer> unionSet = new HashSet<>(list1);
+    unionSet .addAll(list2);
+    return new ArrayList<>(unionSet);
+
+    @Override
+    ist<Integer> intersectionOfStringList(List<Integer> list1, List<Integer> list2);
+    Set<Integer> set1 = new HashSet<>(list1);
+    Set<Integer> set2 = new HashSet<>(list2);
+    set1.retainAll(set2);
+    return new ArrayList<>(set1);
+
+    @Override
+    ist<Integer> differenceOfStringList(List<Integer> list1, List<Integer> list2);
+    Set<Integer> set1 = new HashSet<>(list1);
+    Set<Integer> set2 = new HashSet<>(list2);
+    set1.remoweAll(set2);
+    return new ArrayList<>(set1);
+
+    @Override
+    boolean areStringElementsUnique(List<Integer> list);
+    Set<Integer> uniqueSet = new HashSet<>(list);
+    return list.size() == uniqueSet.size();
+
+}
+*/
