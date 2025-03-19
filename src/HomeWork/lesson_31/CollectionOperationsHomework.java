c87816614c2a7bbe2016945ec491ed45063ffe29
Juri_Bürkle <rastopka@gmx.degit config --global user.email rastopka@gmx.de>
2025-03-19 17:01:07 +0100

    Homework 31

--- src/HomeWork/lesson_31/CollectionOperationsHomework.java
+++ src/HomeWork/lesson_31/CollectionOperationsHomework.java
@@ -1,72 +1,121 @@
-/*
 package Homework.lesson_31;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public interface CollectionOperationsHomework {
 
-    @Override
-    int countDuplicatesInteger(List<Integer> list);
-    Set<Integer> uniqueSet = new HashSet<>(list);
-    return list.size() - uniqueSet.size();
+    /*
+     * Интерфейс для выполнения операций над коллекциями Set и List.
+     * Студенты должны реализовать этот интерфейс, используя базовые операции (например, add, addAll)
+     * для решения задач, связанных с дубликатами и операциями над множествами.
+     *
+     * Примеры тестовых данных:
+     * - Строки: "Apple", "Banana", "Cherry".
+     * - Числа: 1, 2, 3, 4.
+     */
 
-    @Override
+        // Методы для работы со списками целых чисел
+
+        /**
+         * Определяет количество дубликатов в списке целых чисел.
+         * Метод должен использовать Set для удаления дубликатов и вычислять разницу между размером исходного списка
+         * и размером множества уникальных элементов.
+         *
+         * @param list список целых чисел
+         * @return количество дубликатов в списке
+         */
+        int countDuplicatesInteger(List<Integer> list);
+
+        /**
+         * Возвращает объединение двух списков целых чисел, содержащее только уникальные элементы.
+         *
+         * @param list1 первый список целых чисел
+         * @param list2 второй список целых чисел
+         * @return новый список, содержащий объединение list1 и list2
+         */
+        List<Integer> unionOfIntegerLists(List<Integer> list1, List<Integer> list2);
+
+        /**
+         * Возвращает пересечение двух списков целых чисел.
+         *
+         * @param list1 первый список целых чисел
+         * @param list2 второй список целых чисел
+         * @return новый список, содержащий пересечение list1 и list2
+         */
+        List<Integer> intersectionOfIntegerLists(List<Integer> list1, List<Integer> list2);
+
+        /**
+         * Возвращает разность двух списков целых чисел (элементы из list1, которых нет в list2).
+         *
+         * @param list1 первый список целых чисел
+         * @param list2 второй список целых чисел
+         * @return новый список, содержащий разность list1 и list2
+         */
+        List<Integer> differenceOfIntegerLists(List<Integer> list1, List<Integer> list2);
+
     List<Integer> intersectionOfIntegerList(List<Integer> list1, List<Integer> list2);
-    Set<Integer> unionSet = new HashSet<>(list1);
-    unionSet .addAll(list2);
-    return new ArrayList<>(unionSet);
 
-    @Override
-    ist<Integer> intersectionOfIntegerList(List<Integer> list1, List<Integer> list2);
-    Set<Integer> set1 = new HashSet<>(list1);
-    Set<Integer> set2 = new HashSet<>(list2);
-    set1.retainAll(set2);
-    return new ArrayList<>(set1);
+    List<Integer> unionOfIntegerList(List<Integer> list1, List<Integer> list2);
 
-    @Override
-    ist<Integer> differenceOfIntegerList(List<Integer> list1, List<Integer> list2);
-    Set<Integer> set1 = new HashSet<>(list1);
-    Set<Integer> set2 = new HashSet<>(list2);
-    set1.remoweAll(set2);
-    return new ArrayList<>(set1);
+    List<Integer> differenceOfIntegerList(List<Integer> list1, List<Integer> list2);
 
-    @Override
-    boolean areIntegerElementsUnique(List<Integer> list);
-    Set<Integer> uniqueSet = new HashSet<>(list);
-    return list.size() == uniqueSet.size();
+    /**
+         * Проверяет, содержатся ли в списке целых чисел только уникальные элементы.
+         *
+         * @param list список целых чисел
+         * @return true, если все элементы уникальны, иначе false
+         */
+        boolean areIntegerElementsUnique(List<Integer> list);
 
-    @Override
-    int countDuplicatesString(List<Integer> list);
-    Set<Integer> uniqueSet = new HashSet<>(list);
-    return list.size() - uniqueSet.size();
 
-    @Override
-    List<Integer> intersectionOfStringList(List<Integer> list1, List<Integer> list2);
-    Set<Integer> unionSet = new HashSet<>(list1);
-    unionSet .addAll(list2);
-    return new ArrayList<>(unionSet);
+        // Методы для работы со списками строк
 
-    @Override
-    ist<Integer> intersectionOfStringList(List<Integer> list1, List<Integer> list2);
-    Set<Integer> set1 = new HashSet<>(list1);
-    Set<Integer> set2 = new HashSet<>(list2);
-    set1.retainAll(set2);
-    return new ArrayList<>(set1);
+        /**
+         * Определяет количество дубликатов в списке строк.
+         *
+         * @param list список строк
+         * @return количество дубликатов в списке
+         */
+        int countDuplicatesString(List<String> list);
 
-    @Override
-    ist<Integer> differenceOfStringList(List<Integer> list1, List<Integer> list2);
-    Set<Integer> set1 = new HashSet<>(list1);
-    Set<Integer> set2 = new HashSet<>(list2);
-    set1.remoweAll(set2);
-    return new ArrayList<>(set1);
+        /**
+         * Возвращает объединение двух списков строк, содержащее только уникальные элементы.
+         *
+         * @param list1 первый список строк
+         * @param list2 второй список строк
+         * @return новый список, содержащий объединение list1 и list2
+         */
+        List<String> unionOfStringLists(List<String> list1, List<String> list2);
 
-    @Override
-    boolean areStringElementsUnique(List<Integer> list);
-    Set<Integer> uniqueSet = new HashSet<>(list);
-    return list.size() == uniqueSet.size();
+        /**
+         * Возвращает пересечение двух списков строк.
+         *
+         * @param list1 первый список строк
+         * @param list2 второй список строк
+         * @return новый список, содержащий пересечение list1 и list2
+         */
+        List<String> intersectionOfStringLists(List<String> list1, List<String> list2);
 
+        /**
+         * Возвращает разность двух списков строк (элементы из list1, которых нет в list2).
+         *
+         * @param list1 первый список строк
+         * @param list2 второй список строк
+         * @return новый список, содержащий разность list1 и list2
+         */
+        List<String> differenceOfStringLists(List<String> list1, List<String> list2);
+
+    List<String> intersectionOfStringList(List<String> list1, List<String> list2);
+
+    List<String> unionOfStringList(List<String> list1, List<String> list2);
+
+    List<String> differenceOfStringList(List<String> list1, List<String> list2);
+
+    /**
+         * Проверяет, содержатся ли в списке строк только уникальные элементы.
+         *
+         * @param list список строк
+         * @return true, если все элементы уникальны, иначе false
+         */
+        boolean areStringElementsUnique(List<String> list);
 }
-*/
