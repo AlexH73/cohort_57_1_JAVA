f0551341298047a0bca3a974683ed83260a9493a
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-03-20 20:14:26 +0100

    урок 32

--- src/Lessons/Lesson_33/practice/CollectionTasksImpl.java
+++ src/Lessons/Lesson_33/practice/CollectionTasksImpl.java
@@ -2,7 +2,7 @@ package Lessons.Lesson_33.practice;
 
 import java.util.List;
 import java.util.Map;
-=import java.util.Queue;
+import java.util.Queue;
 
 /**
  * Реализация интерфейса CollectionTasks.
