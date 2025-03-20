86e8ee55c607338c81271721b54eaa406c4dc8fd
Juri_Bürkle <rastopka@gmx.degit config --global user.email rastopka@gmx.de>
2025-03-20 16:50:52 +0100

    HomeWork 32/0

---
+++ src/HomeWork/lesson_32/BracketValidatorImpl.java
@@ -0,0 +1,24 @@
+package Homework.lesson_32;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BracketValidatorImpl implements BracketValidator {
+    private Map<String, String> bracketValidater = new HashMap<>();
+
+    @Override
+    public boolean isValidBracketSequence(String input) {
+        return false;
+    }
+
+    @Override
+    public List<Integer> getInvalidBracketIndexes(String input) {
+        return List.of();
+    }
+
+    @Override
+    public String fixBracketSequence(String input) {
+        return "";
+    }
+}
