4f8c5d5d46434137bef3d5ae9c40017d844575c3
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-03-25 11:18:36 +0100

    урок 34.
    Обработка ошибок и исключений

---
+++ src/Lessons/Lesson_34/code/student_code/StudentCode.java
@@ -0,0 +1,8 @@
+package Lessons.Lesson_34.code.student_code;
+
+public class StudentCode {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Lessons/Lesson_34/code/teacher_code/TeacherCode.java
@@ -0,0 +1,11 @@
+package Lessons.Lesson_34.code.teacher_code;
+
+public class TeacherCode {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
