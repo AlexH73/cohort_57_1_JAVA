cfa4db2015eb1f286f2a9d1a4c3d9d9d6903f0af
Alex_H <ewebotah@gmail.com>
2025-03-27 21:35:01 +0100

    Реализован метод copyWithLineNumbers

--- src/Lessons/Lesson_35/src/practice/classwork/LineNumberingFileCopierImpl.java
+++ src/Lessons/Lesson_35/src/practice/classwork/LineNumberingFileCopierImpl.java
@@ -6,6 +6,18 @@ import java.io.*;
  * Реализация задачи 1: Копирование текстового файла с добавлением номера строки.
  */
 public class LineNumberingFileCopierImpl implements LineNumberingFileCopier {
+    public static void main(String[] args) {
+        String input = "src/Lessons/Lesson_35/src/teacher_code/file.txt";
+        String output = "src/Lessons/Lesson_35/src/practice/classwork/newFile.txt";
+
+        LineNumberingFileCopierImpl lnfc = new LineNumberingFileCopierImpl();
+
+        try {
+            lnfc.copyWithLineNumbers(input, output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Читает содержимое текстового файла, добавляет к каждой строке её порядковый номер
@@ -24,5 +36,36 @@ public class LineNumberingFileCopierImpl implements LineNumberingFileCopier {
         // Шаг 3. Создать счетчик строк и читать файл построчно
         // Шаг 4. Для каждой строки записывать в формате "номер. строка"
         // Шаг 5. Закрыть все потоки в блоке finally (или использовать try-with-resources)
+        File readFile = new File(inputFilePath);
+        File writFile = new File(outputFilePath);
+
+        if (!writFile.exists()) {
+            writFile.createNewFile();
+        }
+
+        FileReader reader = new FileReader(readFile);
+        FileWriter writer = new FileWriter(writFile);
+
+        BufferedReader bfReader = new BufferedReader(reader);
+        BufferedWriter bfWriter = new BufferedWriter(writer);
+
+        try {
+            int counter = 1;
+            String line = bfReader.readLine();
+
+            while (line != null) {
+                String string = counter + ". " + line + "\n";
+                bfWriter.write(string);
+
+                counter++;
+
+                line = bfReader.readLine();
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            bfReader.close();
+            bfWriter.close();
+        }
     }
 }
