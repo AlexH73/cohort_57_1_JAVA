0b51f2676a1480244fe9e031d9cf905b1f0b77f5
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-03-27 13:02:07 +0100

    урок 35

---
+++ src/Lessons/Lesson_35/src/student_code/StudentCode35.java
@@ -0,0 +1,7 @@
+package Lessons.Lesson_35.src.student_code;
+
+public class StudentCode35 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Lessons/Lesson_35/src/teacher_code/TeacherCode35.java
@@ -0,0 +1,12 @@
+package Lessons.Lesson_35.src.teacher_code;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class TeacherCode35 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
