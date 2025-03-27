b1c9696224407ab1ad91f372942e45fc9e1c0f57
Alexander_Gott <gott_ii@gmx.de>
2025-03-27 19:29:12 +0100

    StudentCode35

--- src/Lessons/Lesson_35/src/student_code/StudentCode35.java
+++ src/Lessons/Lesson_35/src/student_code/StudentCode35.java
@@ -1,7 +1,10 @@
 package Lessons.Lesson_35.src.student_code;
 
+import java.io.File;
+
 public class StudentCode35 {
     public static void main(String[] args) {
+        File dir = new File(src\Lessons\Lesson_35);
 
     }
 }
