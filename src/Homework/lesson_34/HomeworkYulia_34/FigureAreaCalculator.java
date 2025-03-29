61a4db8335744a505fa2e01c9d8704e59fb458a6
Yulia Kashina <yulia.kaot@mail.ru>
2025-03-29 12:42:31 +0100

    HomeworkYulia 34 is done

---
+++ src/Homework/lesson_34/HomeworkYulia_34/FigureAreaCalculator.java
@@ -0,0 +1,7 @@
+package Homework.lesson_34.HomeworkYulia_34;
+
+import java.util.List;
+
+public interface FigureAreaCalculator {
+    double calculateArea(List<Double> sides) throws IncorrectFigureSizeException;
+}
---
+++ src/Homework/lesson_34/HomeworkYulia_34/IncorrectFigureSizeException.java
@@ -0,0 +1,7 @@
+package Homework.lesson_34.HomeworkYulia_34;
+
+public class IncorrectFigureSizeException extends Throwable {
+    public IncorrectFigureSizeException(String message) {
+        super();
+    }
+}
