4cba8356a1808afed214eb3ff6390516790b1ff1
Yulianna-B <juliburkovska@gmail.com>
2025-03-30 19:08:30 +0200

    домашнее задание 34

--- src/Homework/lesson_34/IncorrectFigureSizeException.java
+++ src/Homework/lesson_34/IncorrectFigureSizeException.java
@@ -2,7 +2,7 @@ package Homework.lesson_34;
 
 public class IncorrectFigureSizeException extends Exception {
     public IncorrectFigureSizeException(String message) {
-        super(message);
+        super(message  );
     }
 
 }
