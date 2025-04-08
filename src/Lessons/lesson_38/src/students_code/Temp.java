5f4bc36b687504d90df25db418af2c8124d3a66f
Yulianna-B <juliburkovska@gmail.com>
2025-04-08 22:21:31 +0200

    debug commit to YULIANNA_BURKOVSKA branch

---
+++ src/Lessons/lesson_38/src/students_code/Temp.java
@@ -0,0 +1,7 @@
+package Lessons.lesson_38.src.students_code;
+
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println("temp file to check git push to current brunch");
+    }
+}
