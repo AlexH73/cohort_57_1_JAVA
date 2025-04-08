ebd229d7ba153f616a9c42be363ca8d2ff23c568
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-04-08 21:46:58 +0200

    lesson 38

--- src/Lessons/lesson_38/src/teachers_code/TeacherSandbox38_3.java
+++ src/Lessons/lesson_38/src/teachers_code/TeacherSandbox38_3.java
@@ -34,13 +34,12 @@ public class TeacherSandbox38_3 {
 
         System.out.println("sorted:");
         int[] sorted3 = {0, 1, 2, 3, 4, 5};
-        bubbleSort(sorted3);
+        easyBubbleSort(sorted3);
 
         System.out.println("\n\n=============================\nnot sorted:");
         int[] notSorted3 = {20, 0, 1, 2, 3, 4, 5};
-        bubbleSort(notSorted3);
+        easyBubbleSort(notSorted3);
     }
-
 
     private static boolean isSorted(int[] nums) {
         // not null not empty...
@@ -74,6 +73,25 @@ public class TeacherSandbox38_3 {
                break;
             }
         }
+        System.out.println("nums.length = " + nums.length);
+        System.out.println("counter = " + counter);
+    }
+
+    private static void easyBubbleSort(int[] nums) {
+        int counter = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(Arrays.toString(nums));
+            for (int j = 1; j < nums.length; j++) {
+                counter++;
+                if (nums[j - 1] > nums[j]) {
+                    int temp = nums[j - 1];
+                    nums[j - 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
         System.out.println("nums.length = " + nums.length);
         System.out.println("counter = " + counter);
     }
