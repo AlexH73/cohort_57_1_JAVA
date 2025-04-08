429994eb8012bbc493bc339578f0d9a3dac035ac
Iryna_Haber <gaber.ira@gmail.com>
2025-04-08 21:05:34 +0200

    #Практика

--- src/Lessons/lesson_38/src/students_code/StudentSandbox38.java
+++ src/Lessons/lesson_38/src/students_code/StudentSandbox38.java
@@ -1,11 +1,5 @@
 package Lessons.lesson_38.src.students_code;
 
 public class StudentSandbox38 {
-    public static void main(String[] args) {
 
-    }
-
-    static boolean isSorted() {
-
-    }
 }
