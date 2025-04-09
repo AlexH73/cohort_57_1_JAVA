9a1e794564bc6215365795aaa9909f6efc117721
Alex_H <ewebotah@gmail.com>
2025-04-10 01:52:42 +0200

    Немного практики

--- src/Practice/search_and_sort/LinearSearch.java
+++ src/Practice/search_and_sort/LinearSearch.java
@@ -1,24 +1,39 @@
 package Practice.search_and_sort;
 
-/**
- * 📌 Условие:
- * Дан массив целых чисел и число target. Найди индекс этого числа в массиве. Если не найден — верни -1.
- */
+
 public class LinearSearch {
     public static void main(String[] args) {
         int[] nums = {10, 20, 30, 40, 50};
         int target = 30;
-        System.out.println("Индекс: " + liniarSearch(nums, target));// Ожидаемый результат: 2
+        System.out.println("Индекс: " + searchIndex(nums, target));// Ожидаемый результат: 2
+        System.out.println("Присутствует: " + target + " в массиве? " + (presenceOfElement(nums, target)? "Да" : "Нет")); // Ожидаемый результат: Да
 
 
     }
 
-    public static int liniarSearch(int[] arr, int target) {
+    /**
+     * 📌 Условие:
+     * Дан массив целых чисел и число target. Найди индекс этого числа в массиве. Если не найден — верни -1.
+     */
+    public static int searchIndex (int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 return i;
             }
         }
         return -1;
+    }
+
+    /**
+     * 📌 Условие:
+     * Проверь, существует ли число target в массиве.
+     */
+    public static boolean presenceOfElement(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return true;
+            }
+        }
+        return false;
     }
 }
