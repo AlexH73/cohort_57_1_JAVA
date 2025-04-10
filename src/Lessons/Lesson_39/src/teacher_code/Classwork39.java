650e8e60c406ddd1252426790d52832ac6f1cdbe
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-04-10 21:22:48 +0200

    fibonacci

---
+++ src/Lessons/Lesson_39/src/teacher_code/Classwork39.java
@@ -0,0 +1,17 @@
+package Lessons.Lesson_39.src.teacher_code;
+
+public class Classwork39 {
+    public static void main(String[] args) {
+        //3 -> 1 + (1 + 2 + 3) -> 1 + 1 + (1 + 1) + (1 + 2)
+        // 5 = 1 + (1 + 2 + 3 + 5 + 8) -> 20
+        // 6 = 1 + (1 + 2 + 3 + 5 + 8 + 13) -> 33
+        // 1 -> 1
+        // 2 -> 2
+        // 0 и меньше -> -1;
+    }
+
+    static int fibonacci(int number) {
+
+        return  -1;
+    }
+}
