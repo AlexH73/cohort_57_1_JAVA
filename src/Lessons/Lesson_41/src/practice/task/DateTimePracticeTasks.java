5eac309e80d3cfc926234f58e7a65b317c538271
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-04-13 18:06:17 +0200

    lesson 41

---
+++ src/Lessons/Lesson_41/src/practice/task/DateTimePracticeTasks.java
@@ -0,0 +1,51 @@
+package Lessons.Lesson_41.src.practice.task;
+
+public class DateTimePracticeTasks {
+    public static void main(String[] args) {
+        System.out.println("Age: " + calculateAge("1990-01-01"));
+        System.out.println("Minutes between: " + timeBetween("09:00:00", "10:30:00"));
+        System.out.println("Day of week: " + findDayOfWeek("2023-01-01"));
+        System.out.println("Formatted current date and time: " + formatCurrentDateTime());
+    }
+
+    /**
+     * Задача 1: Рассчитайте возраст человека на основе его даты рождения.
+     *
+     * @param birthdate Дата рождения человека в формате "yyyy-MM-dd".
+     * @return Возраст человека в годах.
+     */
+    public static int calculateAge(String birthdate) {
+        return 0;
+    }
+
+    /**
+     * Задача 2: Определите, сколько времени прошло между двумя моментами времени в течение дня.
+     *
+     * @param startTime Время начала в формате "HH:mm:ss".
+     * @param endTime Время окончания в формате "HH:mm:ss".
+     * @return Продолжительность между двумя моментами времени в минутах.
+     */
+    public static long timeBetween(String startTime, String endTime) {
+        return 0;
+    }
+
+    /**
+     * Задача 3: Найдите день недели для даты.
+     *
+     * @param date Дата в формате "yyyy-MM-dd".
+     * @return День недели в текстовом формате (например, "Понедельник").
+     */
+    public static String findDayOfWeek(String date) {
+        return null;
+    }
+
+    /**
+     * Задача 4: Форматируйте текущую дату и время в красивом формате.
+     *
+     * @return Текущая дата и время в формате "dd MMMM yyyy, EEEE HH:mm:ss".
+     */
+    public static String formatCurrentDateTime() {
+        return null;
+    }
+}
+
---
+++ src/Lessons/Lesson_41/src/student_code/StudentCode41.java
@@ -0,0 +1,7 @@
+package Lessons.Lesson_41.src.student_code;
+
+public class StudentCode41 {
+    public static void main(String[] args) {
+
+    }
+}
