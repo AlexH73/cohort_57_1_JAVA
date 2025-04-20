a90d49743f09fcc9ffe551263665ee5981361421
Yulia Kashina <yulia.kaot@mail.ru>
2025-04-20 21:00:32 +0200

    H/w 40 is done

--- src/Homework/lesson_40/ColorImageGenerator.java
+++ src/Homework/lesson_40/ColorImageGenerator.java
@@ -21,9 +21,18 @@ import java.io.IOException;
 public class ColorImageGenerator {
 
     public static String rgb(int r, int g, int b) {
-        // TODO: реализовать метод
-        return null;
+        if (r < 0) r = 0;
+        if (r > 255) r = 255;
+
+        if (g < 0) g = 0;
+        if (g > 255) g = 255;
+
+        if (b < 0) b = 0;
+        if (b > 255) b = 255;
+
+        return String.format("%02X%02X%02X", r, g, b);
     }
+
 
     /**
      * Метод создаёт картинку 300x300 пикселей с фоном заданного цвета.
