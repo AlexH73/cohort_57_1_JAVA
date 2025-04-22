01495e4e57d4b35398a72b581a57cf6625f3d085
Alexander_Gott <gott_ii@gmx.de>
2025-04-22 22:03:22 +0200

    Invoice_42 fertig

--- src/Lessons/Lesson_42/src/practice/task_srp/Invoice.java
+++ src/Lessons/Lesson_42/src/practice/task_srp/Invoice.java
@@ -2,9 +2,25 @@ package Lessons.Lesson_42.src.practice.task_srp;
 
 // — Распишите, как вы разделите этот класс на несколько, чтобы каждый отвечал только за одно действие.
 class Invoice {
-    void calculateTotal() {}
+    private int summeAmount;
+    private int konto;
+    private String description;
 
-    void printInvoice() { }
+    public Invoice(int summeAmount, int konto, String description) {
+        this.summeAmount = summeAmount;
+        this.konto = konto;
+        this.description = description;
+    }
 
-    void saveToFile(String path) { }
+    double calculateTotal() {
+        return  ((double) summeAmount) / 100;
+    }
+
+    private void printInvoice() {
+        System.out.println(" Wurde überwiesen: " + summeAmount + " " + konto + " " + description);
+    }
+
+    void saveToFile(String path) {
+        System.out.println(" Information wurde gespeichert in File .....");
+    }
 }
