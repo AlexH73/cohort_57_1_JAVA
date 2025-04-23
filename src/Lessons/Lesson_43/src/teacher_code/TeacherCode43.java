7f66827762ab88e7b63e2210e6f5564bc8152974
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-04-23 19:21:28 +0200

    урок 43

--- src/Lessons/Lesson_43/src/teacher_code/TeacherCode43.java
+++ src/Lessons/Lesson_43/src/teacher_code/TeacherCode43.java
@@ -4,8 +4,15 @@ import static java.lang.Math.*;
 
 public class TeacherCode43 {
     public static void main(String[] args) {
-
+        TextBuch textBuch = new TextBuch();
+        resultGenerator(textBuch);
     }
 
+    private static int resultGenerator(Buch buch){
+        buch.getBuch();
+
+        // temporary stub:
+        return -1;
+    }
 
 }
