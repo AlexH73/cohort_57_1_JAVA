7743d8dba472712db9e2655eb8af664541d1b646
Alex_H <ewebotah@gmail.com>
2025-04-23 22:43:21 +0200

    Task1 is done

--- src/Lessons/Lesson_43/src/classwork/Task1.java
+++ src/Lessons/Lesson_43/src/classwork/Task1.java
@@ -1,24 +1,31 @@
 package Lessons.Lesson_43.src.classwork;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Task1 {
     public static void main(String[] args) {
-
-
         List<Integer> nums = List.of(1, 2, 3, -4, 0, -5);
 
+        // Функциональный подход с Predicate<Integer>:
+        Predicate<Integer> isPositivePredicate = n -> n > 0;
+
+        // Проверка всех элементов массива
+        boolean allPositive = nums.stream().allMatch(isPositivePredicate);
+
+        System.out.println("Все числа положительные? " + (allPositive ? "Да" : "Нет"));
+
+        // Пример проверки каждого элемента
+        nums.stream()
+                .forEach(n -> System.out.println(n + " положительное? " + (isPositivePredicate.test(n) ? "Да" : "Нет")));
+
+        // Императивный подход с методом isPositive:
         for (Integer num : nums) {
-            System.out.println("isPositive(num) = " + isPositive(num));
+            System.out.println(num + " положительное? " + (isPositive(num) ? "Да" : "Нет"));
         }
     }
 
-
     static boolean isPositive(int n) {
-        if (n > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return n > 0;
     }
 }
