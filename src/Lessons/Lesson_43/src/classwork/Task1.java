4953a13a3ac230f8e17ed1bdf4ab7efeb902e77c
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-04-23 21:11:07 +0200

    урок 43

--- src/Lessons/Lesson_43/src/classwork/Task1.java
+++ src/Lessons/Lesson_43/src/classwork/Task1.java
@@ -6,7 +6,9 @@ public class Task1 {
     public static void main(String[] args) {
         List<Integer> nums = List.of(1, 2, 3, -4, 0, -5);
 
-
+        for (Integer num : nums) {
+            System.out.println("isPositive(num) = " + isPositive(num));
+        }
     }
 
 
