c70f8ab66a8bec32471ba26b0e88b72e98f82c02
Alex_H <ewebotah@gmail.com>
2025-04-23 22:06:21 +0200

    point

---
+++ src/Lessons/Lesson_43/src/classwork/Task2.java
@@ -0,0 +1,28 @@
+package Lessons.Lesson_43.src.classwork;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class Task2 {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Java", "Hi", "Stream");
+
+        Function<String, Integer>  = (words) -> {
+
+
+            for (String s : raw) {
+                System.out.println("'" + pipeline.apply(s) + "'");
+            }
+        };
+    }
+
+    public static int lengthOf(String words) {
+        for (String s : words) {
+            String str = s;
+        }
+        return str.length();
+    }
+}
+
+
