e887e2ea3696f5c37b1e121c66c6739d7b2d159b
Alexander_Gott <gott_ii@gmx.de>
2025-04-29 21:14:07 +0200

    Lesson_44_aufgabe_3

--- src/Lessons/Lesson_44/src/practice/classwork/StreamApiPractice01.java
+++ src/Lessons/Lesson_44/src/practice/classwork/StreamApiPractice01.java
@@ -2,6 +2,9 @@ package Lessons.Lesson_44.src.practice.classwork;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class StreamApiPractice01 {
 
@@ -21,13 +24,14 @@ public class StreamApiPractice01 {
      */
     public static void countWordsStartingWithA() {
         List<String> words = List.of("Apple", "Banana", "Avocado", "Cherry", "apricot");
+        long x = words.stream().filter(word -> word.startsWith("A")).count();
         int count = 0;
         for (String word : words) {
             if (word.startsWith("A")) {
                 count++;
             }
         }
-        System.out.println("Count of words starting with 'A': " + count);
+        System.out.println("Count of words starting with 'A': " + count +" und " + x);
     }
 
     /**
@@ -38,13 +42,15 @@ public class StreamApiPractice01 {
      */
     public static void collectEvenNumbers() {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
         List<Integer> evenNumbers = new ArrayList<>();
+        List<Integer> evenNumber = numbers.stream().filter(num -> num % 2 == 0).collect(toList());
         for (Integer num : numbers) {
             if (num % 2 == 0) {
                 evenNumbers.add(num);
             }
         }
-        System.out.println("Even numbers: " + evenNumbers);
+        System.out.println("Even numbers: " + evenNumbers + " " + evenNumber);
     }
 
     /**
@@ -54,7 +60,8 @@ public class StreamApiPractice01 {
      * Требуется применить: filter -> findFirst
      */
     public static void findFirstLongWord() {
-        List<String> words = List.of("hi", "world", "fantastic", "code");
+        List<String> words = List.of("hi", "world", "fantastic", "code", "hbgtf");
+        String word1 = words.stream().filter(word -> word.length() > 5).findFirst().orElse("Not found");
         String result = "Not found";
         for (String word : words) {
             if (word.length() > 5) {
@@ -62,7 +69,7 @@ public class StreamApiPractice01 {
                 break;
             }
         }
-        System.out.println("First long word: " + result);
+        System.out.println("First long word: " + word1);
     }
 
     /**
