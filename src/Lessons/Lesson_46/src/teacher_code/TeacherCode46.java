3e297f747a14c4a73aeb93610bf0caa6ca6044b8
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-05-05 19:27:35 +0200

    урок 47.
    Трехслойная архитектура

--- src/Lessons/Lesson_46/src/teacher_code/TeacherCode46.java
+++ src/Lessons/Lesson_46/src/teacher_code/TeacherCode46.java
@@ -1,16 +1,29 @@
 package Lessons.Lesson_46.src.teacher_code;
 
 
+import org.w3c.dom.ls.LSOutput;
+
 public class TeacherCode46 {
     public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(() -> {
             while (true) {
+                // вызывая любой метод из потока, поток переходит в этот метод и метод выполняется в этом потоке,
+                // то есть один и тот же метод может выполняться одновременно в разных потоках:
                 doSomething();
             }
         });
-
+        // для идентификации потоков можно установить имя потоку, сделать это необходимо перед его запуском
         thread1.setName("My thread");
+
+        // если выполнение потока имеет приоритет, отличный от дефолтного,
+        // то есть более важная операция или менее важная,
+        // то можно установить приоритет (значения от 1 до 10 включительно), где 1 - мин приоритет, 10 - мах приоритет.
+        // Потоки создаются с дефолтным приоритетом 5
         thread1.setPriority(Thread.NORM_PRIORITY);
+
+        // Также, если поток должен быть завершен, как только завершится мейн поток,
+        // то можно пометить его как демон-поток.
+        // Это означает, что данный поток станет фоновым и завершится, как только завершит свою работу поток мейн.
         thread1.setDaemon(true);
 
         thread1.start();
@@ -22,6 +35,7 @@ public class TeacherCode46 {
         if (thread1.isAlive()) {
             System.out.printf("Waiting %s\n", thread1.getName());
             thread1.join(3000);
+            // существует два способа остановть потока
             // thread1.stop();
             thread1.interrupt();
         }
@@ -37,7 +51,7 @@ public class TeacherCode46 {
                         String.valueOf(Thread.currentThread().isDaemon()),
                         i));
             } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                System.err.println("Поток был прерван во время сна");
             }
         }
     }
