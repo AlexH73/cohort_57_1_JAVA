c7a274f0f90e5951323879eeaa193f9ed767b622
Ilyas Miftakhov <imiftakhov@devexperts.com>
2025-05-06 21:19:58 +0200

    урок 47.
    Созданы интерфейсы для репозиториев сообщений и пользователей

--- src/Lessons/Lesson_47/application/data_access/MessageRepositoryInterface.java
+++ src/Lessons/Lesson_47/application/data_access/MessageRepositoryInterface.java
@@ -1,4 +1,16 @@
 package Lessons.Lesson_47.application.data_access;
 
+import Lessons.Lesson_47.application.models.Message;
+import Lessons.Lesson_47.application.models.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MessageRepositoryInterface {
+    boolean addMessage(Message message);
+    List<Message> getMessagesByUser(User user);
+    List<Message> getNoReadMessages();
+    List<Message> getMessageByDate(LocalDate date);
+    void updateMessage(Message message);
+    boolean deleteMessage(Message message);
 }
