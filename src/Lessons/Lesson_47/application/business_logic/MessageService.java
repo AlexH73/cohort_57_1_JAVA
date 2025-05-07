5ead813b16e19c3ae1c95a6e508cdc21359a450d
Götte <gott_ii@gmx.de>
2025-05-07 13:48:57 +0200

    MessageService angefangen

--- src/Lessons/Lesson_47/application/business_logic/MessageService.java
+++ src/Lessons/Lesson_47/application/business_logic/MessageService.java
@@ -1,4 +1,14 @@
 package Lessons.Lesson_47.application.business_logic;
 
+import Lessons.Lesson_47.application.data_access.MessageRepositoryImpl;
+import Lessons.Lesson_47.application.data_access.MessageRepositoryInterface;
+
 public class MessageService {
+    private MessageRepositoryInterface messageRepo = new MessageRepositoryImpl();
+
+    public boolean addMessage(String message) {
+        return false;
+    }
+
+
 }
